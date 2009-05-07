fb2cc711aec102338d5044e4222eca4a2241f7be
Ask Solem <askh@opera.com>
2009-05-07 13:54:55 +0200

    Tests for autodiscovery functionality.

---
+++ celery/tests/test_discovery.py
@@ -0,0 +1,19 @@
+import unittest
+from django.conf import settings
+from celery.discovery import autodiscover
+from celery.task import tasks
+
+
+class TestDiscovery(unittest.TestCase):
+
+    def assertDiscovery(self):
+        apps = autodiscover()
+        self.assertTrue(apps)
+        tasks.autodiscover()
+        self.assertTrue("c.unittest.SomeAppTask" in tasks)
+        self.assertEquals(tasks["c.unittest.SomeAppTask"].run(), 42)
+
+    def test_discovery(self):
+        if "someapp" in settings.INSTALLED_APPS:
+            self.assertDiscovery()
+
