bf4e1df007e85b83e1c31b32c1463a31740b9f9b
Ask Solem <askh@opera.com>
2009-06-08 13:11:54 +0200

    Sphinx docs: Now show if the release is stable or unstable.

--- celery/__init__.py
+++ celery/__init__.py
@@ -5,3 +5,14 @@ __author__ = "Ask Solem"
 __contact__ = "askh@opera.com"
 __homepage__ = "http://github.com/ask/celery/"
 __docformat__ = "restructuredtext"
+
+
+def is_stable_release():
+    return bool(not VERSION[1] % 2)
+
+
+def version_with_meta():
+    meta = "unstable"
+    if is_stable_release():
+        meta = "stable"
+    return "%s (%s)" % (__version__, meta)
