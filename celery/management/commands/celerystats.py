da5f207a40a6421de2afae8d71cf5232c3bfc590
Ask Solem <askh@opera.com>
2009-06-17 14:46:18 +0200

    Report not Dump

--- celery/management/commands/celerystats.py
+++ celery/management/commands/celerystats.py
@@ -10,11 +10,12 @@ from celery.monitoring import StatsCollector
 class Command(BaseCommand):
     """Run the celery daemon."""
     option_list = BaseCommand.option_list
-    help = 'Dump and flush the currently available statistics'
+    help = "Collect/flush and dump a report from the currently available "
+           "statistics"
 
     def handle(self, *args, **options):
         """Handle the management command."""
         stats = StatsCollector()
         print("* Gathering statistics...")
         stats.collect()
-        stats.dump()
+        stats.report()
