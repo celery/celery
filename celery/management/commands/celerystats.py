b2d5bdd225f215f627dfd956c535e2cb079aa5dc
Ask Solem <askh@opera.com>
2009-06-17 14:41:42 +0200

    New management command: celerystats (Dump and flush currently available statistics)

---
+++ celery/management/commands/celerystats.py
@@ -0,0 +1,20 @@
+"""
+
+Start the celery daemon from the Django management command.
+
+"""
+from django.core.management.base import BaseCommand
+from celery.monitoring import StatsCollector
+
+
+class Command(BaseCommand):
+    """Run the celery daemon."""
+    option_list = BaseCommand.option_list + OPTION_LIST
+    help = 'Dump and flush the currently available statistics'
+
+    def handle(self, *args, **options):
+        """Handle the management command."""
+        stats = StatsCollector()
+        print("* Gathering statistics...")
+        stats.collect()
+        stats.dump()
