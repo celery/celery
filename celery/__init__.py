93b13a9f241560c1bf9a999e4b1d2318c47075f5
Ask Solem <askh@opera.com>
2009-06-23 14:21:23 +0200

    Bumped version to 0.3.14

--- celery/__init__.py
+++ celery/__init__.py
@@ -1,5 +1,5 @@
 """Distributed Task Queue for Django"""
-VERSION = (0, 3, 13)
+VERSION = (0, 3, 14)
 __version__ = ".".join(map(str, VERSION))
 __author__ = "Ask Solem"
 __contact__ = "askh@opera.com"
