9fa2c0c76c83dc16990da6bf039f04e963f8bfd1
Ask Solem <askh@opera.com>
2009-06-23 14:50:05 +0200

    Somehow countdown+eta arguments was merged away

--- celery/execute.py
+++ celery/execute.py
@@ -8,7 +8,8 @@ from datetime import datetime, timedelta
 
 def apply_async(task, args=None, kwargs=None, routing_key=None,
         immediate=None, mandatory=None, connection=None,
-        connect_timeout=AMQP_CONNECTION_TIMEOUT, priority=None, **opts):
+        connect_timeout=AMQP_CONNECTION_TIMEOUT, priority=None,
+        countdown=None, eta=None, **opts):
     """Run a task asynchronously by the celery daemon(s).
 
     :param task: The task to run (a callable object, or a :class:`Task`
