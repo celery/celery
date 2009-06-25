169db374af76d248cb45dd3b8af27e95b6b2ba1b
Ask Solem <askh@opera.com>
2009-06-25 16:02:40 +0200

    Unit tests for utils.chunks

---
+++ celery/tests/test_utils.py
@@ -0,0 +1,22 @@
+import unittest
+from celery.utils import chunks
+
+
+class TestChunks(unittest.TestCase):
+
+    def test_chunks(self):
+
+        # n == 2
+        x = chunks(iter([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 2)
+        self.assertEquals(list(x),
+            [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9], [10]])
+
+        # n == 3
+        x = chunks(iter([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 3)
+        self.assertEquals(list(x),
+            [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10]])
+
+        # n == 2 (exact)
+        x = chunks(iter([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]), 2)
+        self.assertEquals(list(x),
+            [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]])
