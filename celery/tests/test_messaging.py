773a389f2ae69384f09cbada8b4b2615a7c430de
Ask Solem <askh@opera.com>
2009-07-02 14:07:34 +0200

    Add regression test for the message options bug.

---
+++ celery/tests/test_messaging.py
@@ -0,0 +1,16 @@
+import unittest
+from celery.messaging import MSG_OPTIONS, get_msg_options, extract_msg_options
+
+
+class TestMsgOptions(unittest.TestCase):
+
+    def test_MSG_OPTIONS(self):
+        self.assertTrue(MSG_OPTIONS)
+
+    def test_extract_msg_options(self):
+        testing = {"mandatory": True, "routing_key": "foo.xuzzy"}
+        result = extract_msg_options(testing)
+        self.assertEquals(result["mandatory"], True)
+        self.assertEquals(result["routing_key"], "foo.xuzzy")
+
+        
