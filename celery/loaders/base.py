9bb62dc1ae4f67b48f4a9cefd051b8e0b3555cfe
Ask Solem <askh@opera.com>
2009-08-15 16:25:32 +0200

    Documented celery.loaders.base

--- celery/loaders/base.py
+++ celery/loaders/base.py
@@ -1,16 +1,34 @@
 
 
 class BaseLoader(object):
+    """The base class for loaders.
+
+    Loaders handles to following things:
+
+        * Reading celery client/worker configurations.
+
+        * What happens when a task starts?
+            See :meth:`on_task_init`.
+
+        * What happens when the worker starts?
+            See :meth:`on_worker_init`.
+
+        * What modules are imported to find tasks?
+
+    """
     _conf_cache = None
 
     def on_task_init(self, task_id, task):
+        """This method is called before a task is executed."""
         pass
 
     def on_worker_init(self):
+        """This method is called when the worker (``celeryd``) starts."""
         pass
 
     @property
     def conf(self):
+        """Loader configuration."""
         if not self._conf_cache:
             self._conf_cache = self.read_configuration()
         return self._conf_cache
