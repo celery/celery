cb68231c20bcb079e9c0213323b2a7d1f3d81b40
Ask Solem <askh@opera.com>
2009-08-15 16:15:18 +0200

    Documented celery.loaders

--- celery/loaders/__init__.py
+++ celery/loaders/__init__.py
@@ -2,9 +2,29 @@ from celery.loaders.djangoapp import Loader as DjangoLoader
 from celery.loaders.default import Loader as DefaultLoader
 from django.conf import settings
 
+"""
+.. class:: Loader
+
+The current loader class.
+
+"""
 Loader = DefaultLoader
 if settings.configured:
     Loader = DjangoLoader
 
+"""
+.. data:: current_loader
+
+The current loader instance.
+
+"""
 current_loader = Loader()
+
+
+"""
+.. data:: settings
+
+The global settings object.
+
+"""
 settings = current_loader.conf
