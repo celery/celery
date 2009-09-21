b3a468ef60334726af6a38cdfdd375e1f687c89a
Ask Solem <askh@opera.com>
2009-09-21 16:13:46 +0200

    Added signal: task_sent, triggered when a task has been sent to the broker.

--- celery/signals.py
+++ celery/signals.py
@@ -1,8 +1,38 @@
 from django.dispatch import Signal
 
-
 """
 
+.. DATA: task_sent
+
+Triggered when a task has been sent to the broker.
+
+Provides arguments:
+
+* task_id
+    Id of the task to be executed.
+
+* task
+    The task being executed.
+
+* args
+    the tasks positional arguments.
+
+* kwargs
+    The tasks keyword arguments.
+
+* eta
+    The time to execute the task.
+
+* taskset
+    Id of the taskset this task is part of (if any).
+
+
+"""
+task_sent = Signal(providing_args=[
+                        "task_id", "task", "args", "kwargs", "eta",
+                        "taskset"])
+
+"""
 .. DATA: task_prerun
 
 Triggered before a task is executed.
