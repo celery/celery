cfe7e161757b1c28afaaf1a6917d09b56d02913b
Ask Solem <askh@opera.com>
2009-09-22 14:05:27 +0200

    Bump version to 0.8.0

--- README.rst
+++ README.rst
@@ -2,7 +2,7 @@
  celery - Distributed Task Queue
 =================================
 
-:Version: 0.7.99
+:Version: 0.8.0
 
 Introduction
 ============
--- celery/__init__.py
+++ celery/__init__.py
@@ -1,6 +1,6 @@
 """Distributed Task Queue"""
 
-VERSION = (0, 7, 99)
+VERSION = (0, 8, 0)
 
 __version__ = ".".join(map(str, VERSION))
 __author__ = "Ask Solem"
