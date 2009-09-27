0622780e222d9550baee8389a4146941381b1ccc
Ask Solem <askh@opera.com>
2009-09-27 16:08:08 +0200

    Ok, making celery.py a shortcut module was a mistake.

--- celery/__init__.py
+++ celery/__init__.py
@@ -2,7 +2,3 @@
 from celery.distmeta import __version__, __author__, __contact__
 from celery.distmeta import __homepage__, __docformat__
 from celery.distmeta import VERSION, is_stable_release, version_with_meta
-
-from celery.decorators import task
-from celery.task.base import Task, PeriodicTask
-from celery.execute import apply_async, apply
