2359b65a59b5326a07768578469177d65bbddf6e
Ask Solem <askh@opera.com>
2009-09-27 16:40:33 +0200

    Use from .. import (...) parens

--- celery/__init__.py
+++ celery/__init__.py
@@ -1,4 +1,4 @@
 """Distributed Task Queue"""
-from celery.distmeta import __version__, __author__, __contact__
-from celery.distmeta import __homepage__, __docformat__
-from celery.distmeta import VERSION, is_stable_release, version_with_meta
+from celery.distmeta import (__version__, __author__, __contact__,
+                             __homepage__, __docformat__, VERSION,
+                             is_stable_release, version_with_meta)
