8e2cb8d67466615be0cca26505923bec429e7880
Ask Solem <askh@opera.com>
2009-11-16 13:52:36 +0100

    Document contrib.test_runner

--- celery/contrib/test_runner.py
+++ celery/contrib/test_runner.py
@@ -6,5 +6,14 @@ Custom test runner to allow testing of celery delayed tasks.
 """
 
 def run_tests(test_labels, *args, **kwargs):
+    """Django test runner allowing testing of celery delayed tasks.
+
+    All tasks are run locally, not in a worker.
+
+    To use this runner set ``settings.TEST_RUNNER``::
+
+        TEST_RUNNER = "celery.contrib.test_runner.run_tests"
+
+    """
     settings.CELERY_ALWAYS_EAGER = True
     return run_tests_orig(test_labels, *args, **kwargs)
