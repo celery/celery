dc7a7141c0cc25c50bd7f2797a64fe275a30a85e
Ask Solem <askh@opera.com>
2009-11-20 15:45:04 +0100

    PEP8ify

--- contrib/test_runner.py
+++ contrib/test_runner.py
@@ -8,4 +8,3 @@ Custom test runner to allow testing of celery .delay() tasks.
 def run_tests(test_labels, *args, **kwargs):
     settings.CELERY_ALWAYS_EAGER = True
     return run_tests_orig(test_labels, *args, **kwargs)
-
