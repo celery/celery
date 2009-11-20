2c3d9a1ff72973eb08e5a361ec705626dcf46b4f
Ask Solem <askh@opera.com>
2009-11-20 15:39:06 +0100

    Only change QOS(prefetch_count) if it has changed

--- celery/worker/__init__.py
+++ celery/worker/__init__.py
@@ -76,8 +76,11 @@ class AMQPListener(object):
 
         self.logger.debug("AMQPListener: Ready to accept tasks!")
 
+        prev_pcount = None
         while True:
-            self.task_consumer.qos(prefetch_count=int(self.prefetch_count))
+            if not prev_pcount or int(self.prefetch_count) != prev_pcount:
+                self.task_consumer.qos(prefetch_count=int(self.prefetch_count))
+                prev_pcount = int(self.prefetch_count)
             it.next()
 
     def stop(self):
