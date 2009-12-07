cb92fb847150d1f8fa3d3a38f852ddbf9b1d45f5
Ask Solem <askh@opera.com>
2009-12-07 13:33:15 +0100

    Cosmetic changes

--- celery/monitoring/__init__.py
+++ celery/monitoring/__init__.py
@@ -6,25 +6,28 @@ from celery.monitoring.web import WebServerThread
 
 
 class MonitorListener(object):
+    """Capture events sent by messages and store them in memory."""
 
     def __init__(self, state):
         self.connection = DjangoBrokerConnection()
         self.receiver = EventReceiver(self.connection, handlers={
-            "worker-heartbeat": state.receive_heartbeat,
-            "worker-online": state.receive_worker_event,
-            "worker-offline": state.receive_worker_event,
             "task-received": state.receive_task_received,
             "task-accepted": state.receive_task_event,
             "task-succeeded": state.receive_task_event,
-            "task-failed": state.receive_task_event,
             "task-retried": state.receive_task_event
+            "task-failed": state.receive_task_event,
+            "worker-online": state.receive_worker_event,
+            "worker-offline": state.receive_worker_event,
+            "worker-heartbeat": state.receive_heartbeat,
         })
 
     def start(self):
-        self.receiver.consume()
+        self.receiver.capture()
 
 
 class MonitorService(object):
+    """celerymon"""
+
 
     def __init__(self, logger, is_detached=False, http_port=8989):
         self.logger = logger
@@ -32,8 +35,5 @@ class MonitorService(object):
         self.http_port = http_port
 
     def start(self):
-        listener = MonitorListener(monitor_state)
-        webthread = WebServerThread(port=self.http_port)
-        webthread.start()
-
-        listener.start()
+        MonitorListener(monitor_state).start()
+        WebServerThread(port=self.http_port).start()
