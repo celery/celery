95522e73835f61b2c8f9bb868da08fb0bfad4a71
Ask Solem <askh@opera.com>
2010-01-27 14:11:15 +0100

    Added new example: examples/celery_http_gateway, a simple http service for applying tasks and querying task results via http.

---
+++ examples/celery_http_gateway/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import *
+
+from celery.task.builtins import PingTask
+from celery.views import task_view
+from celery import views as celery_views
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns("",
+    url(r'^apply/(?P<task_name>.+?)/', celery_views.apply),
+    url(r'^ping/', task_view(PingTask)),
+    url(r'^(?P<task_id>[\w\d\-]+)/done/?$', celery_views.is_task_successful,
+        name="celery-is_task_successful"),
+    url(r'^(?P<task_id>[\w\d\-]+)/status/?$', celery_views.task_status,
+        name="celery-task_status"),
+)
