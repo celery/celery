6b42a2e048663e47a66df5f8941f7f5c533eb1c5
Ask Solem <askh@opera.com>
2010-02-20 17:57:02 +0100

    Make redis backend work with redis-py 1.34.1

--- celery/backends/pyredis.py
+++ celery/backends/pyredis.py
@@ -27,6 +27,7 @@ class RedisBackend(KeyValueStoreBackend):
     redis_host = "localhost"
     redis_port = 6379
     redis_db = "celery_results"
+    redis_password = None
     redis_timeout = None
     redis_connect_retry = None
 
@@ -46,6 +47,9 @@ class RedisBackend(KeyValueStoreBackend):
                             getattr(settings, "REDIS_PORT", self.redis_port)
         self.redis_db = redis_db or \
                             getattr(settings, "REDIS_DB", self.redis_db)
+        self.redis_password = redis_password or \
+                            getattr(settings, "REDIS_PASSWORD",
+                                    self.redis_password)
         self.redis_timeout = redis_timeout or \
                             getattr(settings, "REDIS_TIMEOUT",
                                     self.redis_timeout)
@@ -74,8 +78,7 @@ class RedisBackend(KeyValueStoreBackend):
             self._connection = redis.Redis(host=self.redis_host,
                                     port=self.redis_port,
                                     db=self.redis_db,
-                                    timeout=self.redis_timeout,
-                                    retry_connection=self.redis_connect_retry)
+                                    password=self.redis_password)
             self._connection.connect()
         return self._connection
 
