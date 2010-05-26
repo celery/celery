86066890322e3c3654946a49c8d1cd2e1a1c2980
Ask Solem <askh@opera.com>
2010-05-26 09:19:58 +0200

    tests.backends: Test using DatabaseBackend instead of RedisBackend, as the latter requires the redis module to be installed.

--- celery/tests/test_backends/__init__.py
+++ celery/tests/test_backends/__init__.py
@@ -2,14 +2,14 @@ import unittest2 as unittest
 
 from celery import backends
 from celery.backends.amqp import AMQPBackend
-from celery.backends.pyredis import RedisBackend
+from celery.backends.database import DatabaseBackend
 
 
 class TestBackends(unittest.TestCase):
 
     def test_get_backend_aliases(self):
         expects = [("amqp", AMQPBackend),
-                   ("redis", RedisBackend)]
+                   ("database", DatabaseBackend)]
         for expect_name, expect_cls in expects:
             self.assertIsInstance(backends.get_backend_cls(expect_name)(),
                                   expect_cls)
