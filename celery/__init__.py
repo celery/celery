e2871034063d1865295bb5a061d55fc8eaa46b6c
Ask Solem <askh@opera.com>
2010-06-07 18:14:24 +0200

    Changed my contact e-mail address.

--- celery/__init__.py
+++ celery/__init__.py
@@ -4,7 +4,7 @@ VERSION = (1, 1, 1)
 
 __version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
 __author__ = "Ask Solem"
-__contact__ = "askh@opera.com"
+__contact__ = "ask@celeryproject.org"
 __homepage__ = "http://github.com/ask/celery/"
 __docformat__ = "restructuredtext"
 
