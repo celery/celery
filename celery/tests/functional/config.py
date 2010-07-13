d7b846f445c0d50edf2e4221cfe4b213d96f9055
Ask Solem <askh@opera.com>
2010-07-13 14:31:30 +0200

    celery.tests.functional: Functional test suite added.

---
+++ celery/tests/functional/config.py
@@ -0,0 +1,28 @@
+import atexit
+import os
+
+CARROT_BACKEND = os.environ.get("CARROT_BACKEND") or "amqplib"
+
+BROKER_HOST = os.environ.get("BROKER_HOST") or "localhost"
+BROKER_USER = os.environ.get("BROKER_USER") or "guest"
+BROKER_PASSWORD = os.environ.get("BROKER_PASSWORD") or "guest"
+BROKER_VHOST = os.environ.get("BROKER_VHOST") or "/"
+
+
+CELERY_RESULT_BACKEND = "amqp"
+CELERY_SEND_TASK_ERROR_EMAILS = False
+
+CELERY_DEFAULT_QUEUE = "testcelery"
+CELERY_DEFAULT_EXCHANGE = "testcelery"
+CELERY_DEFAULT_ROUTING_KEY = "testcelery"
+CELERY_QUEUES = {"testcelery": {"binding_key": "testcelery"}}
+
+CELERYD_LOG_COLOR = False
+
+CELERY_IMPORTS = ("celery.tests.functional.tasks", )
+
+@atexit.register
+def teardown_testdb():
+    import os
+    if os.path.exists("test.db"):
+        os.remove("test.db")
---
+++ celery/tests/functional/tasks.py
@@ -0,0 +1,23 @@
+import time
+
+from celery.decorators import task
+from celery.task.sets import subtask
+
+
+@task
+def add(x, y):
+    return x + y
+
+
+@task
+def add_cb(x, y, callback=None):
+    result = x + y
+    if callback:
+        return subtask(callback).apply_async(result)
+    return result
+
+
+@task
+def sleeptask(i):
+    time.sleep(i)
+    return i
