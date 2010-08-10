0bcd57f5fd5748a1744f99c12987d426bb6d58e2
Ask Solem <askh@opera.com>
2010-08-10 19:35:55 +0200

    Redis database *number* not *name*. Default is now 0

--- celery/backends/pyredis.py
+++ celery/backends/pyredis.py
@@ -30,7 +30,7 @@ class RedisBackend(KeyValueStoreBackend):
     """
     redis_host = "localhost"
     redis_port = 6379
-    redis_db = "celery_results"
+    redis_db = 0
     redis_password = None
     redis_timeout = None
     redis_connect_retry = None
