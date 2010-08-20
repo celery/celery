4fe9beb4895c9c86a2fbd841836e1f421e347941
Ask Solem <askh@opera.com>
2010-08-20 13:43:31 +0200

    Change copyright date to 2009 -> 2009-2010. And "Ask Solem" -> "Ask Solem & contributors"

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Celery'
-copyright = u'2009, Ask Solem'
+copyright = u'2009-2010, Ask Solem & contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
