a6fe8a36771cf532c04f1910efbd1503e03026ca
Ask Solem <askh@opera.com>
2010-08-24 15:33:01 +0200

    Bumped version to 2.1.0a2

--- celery/__init__.py
+++ celery/__init__.py
@@ -1,6 +1,6 @@
 """Distributed Task Queue"""
 
-VERSION = (2, 1, 0, "a1")
+VERSION = (2, 1, 0, "a2")
 
 __version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
 __author__ = "Ask Solem"
