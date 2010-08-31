ec4951b3787dbebbbd4b2cfee78883667da319de
Ask Solem <askh@opera.com>
2010-08-31 13:57:52 +0200

    Fixed failing tests

--- celery/tests/test_states.py
+++ celery/tests/test_states.py
@@ -1,23 +1,29 @@
 import unittest2 as unittest
 
+from celery.states import state
 from celery import states
 
 
 class test_state_precedence(unittest.TestCase):
 
     def test_gt(self):
-        self.assertGreater(states.SUCCESS, states.PENDING)
-        self.assertGreater(states.FAILURE, states.RECEIVED)
-        self.assertGreater(states.REVOKED, states.STARTED)
-        self.assertGreater(states.SUCCESS, states.state("CRASHED"))
-        self.assertGreater(states.FAILURE, states.state("CRASHED"))
-        self.assertFalse(states.REVOKED > states.state("CRASHED"))
+        self.assertGreater(state(states.SUCCESS),
+                           state(states.PENDING))
+        self.assertGreater(state(states.FAILURE),
+                           state(states.RECEIVED))
+        self.assertGreater(state(states.REVOKED),
+                           state(states.STARTED))
+        self.assertGreater(state(states.SUCCESS),
+                           state("CRASHED"))
+        self.assertGreater(state(states.FAILURE),
+                           state("CRASHED"))
+        self.assertFalse(state(states.REVOKED) > state("CRASHED"))
 
     def test_lt(self):
-        self.assertLess(states.PENDING, states.SUCCESS)
-        self.assertLess(states.RECEIVED, states.FAILURE)
-        self.assertLess(states.STARTED, states.REVOKED)
-        self.assertLess(states.state("CRASHED"), states.SUCCESS)
-        self.assertLess(states.state("CRASHED"), states.FAILURE)
-        self.assertTrue(states.REVOKED < states.state("CRASHED"))
+        self.assertLess(state(states.PENDING), state(states.SUCCESS))
+        self.assertLess(state(states.RECEIVED), state(states.FAILURE))
+        self.assertLess(state(states.STARTED), state(states.REVOKED))
+        self.assertLess(state("CRASHED"), state(states.SUCCESS))
+        self.assertLess(state("CRASHED"), state(states.FAILURE))
+        self.assertTrue(state(states.REVOKED) < state("CRASHED"))
 
