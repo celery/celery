b03300548288637077f57c7bfdae5ca5a10c67b5
Ask Solem <askh@opera.com>
2010-09-29 11:19:38 +0200

    PEP8ify

--- docs/_ext/celerydocs.py
+++ docs/_ext/celerydocs.py
@@ -1,21 +1,21 @@
 def setup(app):
     app.add_crossref_type(
-        directivename = "setting",
-        rolename      = "setting",
-        indextemplate = "pair: %s; setting",
+        directivename="setting",
+        rolename="setting",
+        indextemplate="pair: %s; setting",
     )
     app.add_crossref_type(
-        directivename = "sig",
-        rolename      = "sig",
-        indextemplate = "pair: %s; sig",
+        directivename="sig",
+        rolename="sig",
+        indextemplate="pair: %s; sig",
     )
     app.add_crossref_type(
-        directivename = "state",
-        rolename      = "state",
-        indextemplate = "pair: %s; state",
+        directivename="state",
+        rolename="state",
+        indextemplate="pair: %s; state",
     )
     app.add_crossref_type(
-        directivename = "control",
-        rolename      = "control",
-        indextemplate = "pair: %s; control",
+        directivename="control",
+        rolename="control",
+        indextemplate="pair: %s; control",
     )
