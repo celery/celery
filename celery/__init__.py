ae42674167e54a7f4499ceba841417f9d0a817fe
Ask Solem <askh@opera.com>
2010-10-08 10:48:20 +0200

    Changed __homepage__ to http://celeryproject.org

--- celery/__init__.py
+++ celery/__init__.py
@@ -5,5 +5,5 @@ VERSION = (2, 1, 0, "rc4")
 __version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
 __author__ = "Ask Solem"
 __contact__ = "ask@celeryproject.org"
-__homepage__ = "http://github.com/ask/celery/"
+__homepage__ = "http://celeryproject.org"
 __docformat__ = "restructuredtext"
