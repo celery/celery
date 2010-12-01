4b314f37c6b2d294478f06f9616e55a0ac7a1dab
Ask Solem <askh@opera.com>
2010-12-01 22:00:35 +0100

    Added README.rst to examples/eventlet

--- examples/eventlet/tasks.py
+++ examples/eventlet/tasks.py
@@ -2,13 +2,11 @@ from celery.decorators import task
 from eventlet.green import urllib2
 
 
-@task(ignore_result=True)
+@task
 def urlopen(url):
     print("Opening: %r" % (url, ))
     try:
         body = urllib2.urlopen(url).read()
     except Exception, exc:
-        print("Exception for %r: %r" % (url, exc, ))
-        return url, 0
-    print("Done with: %r" % (url, ))
-    return url, 1
+        print("URL %r gave error: %r" % (url, exc))
+    return len(body)
