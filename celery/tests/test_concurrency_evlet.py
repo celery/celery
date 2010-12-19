a62f27110d9b6b30bcf0ab5e06d7b1fb4e1a5ae3
Ask Solem <ask@celeryproject.org>
2010-12-19 20:56:54 +0100

    Eventlet test failed if eventlet not installed

--- celery/tests/test_concurrency_evlet.py
+++ celery/tests/test_concurrency_evlet.py
@@ -5,14 +5,11 @@ from celery.tests.utils import unittest
 from celery.tests.utils import patch
 
 
-monkey_patched = []
-
-
 class EventletCase(unittest.TestCase):
 
     def setUp(self):
         try:
-            __import__("eventlet")
+            self.eventlet = __import__("eventlet")
         except ImportError:
             raise SkipTest(
                 "eventlet not installed, skipping related tests.")
@@ -21,11 +18,10 @@ class EventletCase(unittest.TestCase):
 
 class test_eventlet_patch(EventletCase):
 
-    def setUp(self):
-        EventletCase.setUp(self)
-
-    @patch("eventlet", "monkey_patch", lambda: monkey_patched.append(True))
-    def test_is_patches(self):
+    def test_is_patched(self):
+        monkey_patched = []
+        prev_monkey_patch = self.eventlet.monkey_patch
+        self.eventlet.monkey_patch = lambda: monkey_patched.append(True)
         prev_evlet = sys.modules.pop("celery.concurrency.evlet", None)
         os.environ.pop("EVENTLET_NOPATCH")
         try:
@@ -35,3 +31,4 @@ class test_eventlet_patch(EventletCase):
         finally:
             sys.modules["celery.concurrency.evlet"] = prev_evlet
             os.environ["EVENTLET_NOPATCH"] = "yes"
+            self.eventlet.monkey_patch = prev_monkey_patch
