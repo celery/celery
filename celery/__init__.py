3fd07db229592ee86981a7f2dd88f05ebc664899
Ask Solem <ask@celeryproject.org>
2011-01-13 15:27:41 +0100

    Deprecates Python 2.4 support\!

--- celery/__init__.py
+++ celery/__init__.py
@@ -1,5 +1,6 @@
 """Distributed Task Queue"""
 import os
+import sys
 
 VERSION = (2, 2, 0, "rc1")
 
@@ -8,6 +9,18 @@ __author__ = "Ask Solem"
 __contact__ = "ask@celeryproject.org"
 __homepage__ = "http://celeryproject.org"
 __docformat__ = "restructuredtext"
+
+
+if sys.version_info < (2, 5):
+    import warnings
+    warnings.warn(DeprecationWarning("""
+
+Python 2.4 support is deprecated and only versions 2.5, 2.6, 2.7+
+will be supported starting from Celery version 2.3.
+
+
+"""))
+
 
 
 def Celery(*args, **kwargs):
