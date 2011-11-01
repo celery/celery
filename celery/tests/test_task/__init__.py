6a9f744a7bb9504cc9c144161667558f1dba19dc
Dan McGee <dan@archlinux.org>
2011-11-01 12:36:46 -0500

    Set task test chord to a more realistic value

--- celery/tests/test_task/__init__.py
+++ celery/tests/test_task/__init__.py
@@ -344,7 +344,7 @@ class TestCeleryTasks(unittest.TestCase):
     def test_after_return(self):
         task = self.createTaskCls("T1", "c.unittest.t.after_return")()
         task.backend = Mock()
-        task.request.chord = {}
+        task.request.chord = return_True_task.subtask()
         task.after_return("SUCCESS", 1.0, "foobar", (), {}, None)
         task.backend.on_chord_part_return.assert_called_with(task)
         task.request.clear()
