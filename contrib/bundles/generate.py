d11833d1725738b12b3a6fa62c86c7e594b25943
Ask Solem <ask@celeryproject.org>
2011-12-01 15:22:33 +0000

    bundles now uses bundle.Bundle.upload

---
+++ contrib/bundles/generate.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(
+    os.path.join(__file__, os.pardir, os.pardir)))
+
+from celery import VERSION
+
+from bundle.gen import Bundle
+
+series = "{}.{}".format(*VERSION[:2])
+
+defaults = {"version": series,
+            "author": "Celery Project",
+            "author_email": "bundles@celeryproject.org",
+            "url": "http://celeryproject.org",
+            "license": "BSD"}
+
+
+bundles = [
+    Bundle("celery-with-redis",
+        "Bundle that installs the dependencies for Celery and Redis",
+        requires=["celery>=%s,<3.0" % (series, ), "redis>=2.4.4"],
+        **defaults),
+]
+
+
+def main():
+    for bundle in bundles:
+        print(bundle.render_readme())
+
+if __name__ == "__main__":
+    main()
