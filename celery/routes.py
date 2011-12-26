e197b6031bfe9e91c0068dd70b2ba28becd873e7
Akira Matsuzaki <akira.matsuzaki.1977@gmail.com>
2011-12-26 21:11:26 +0900

    Support route options on missing queue creation
    Adds an ability to take exchange and routing_key parameters from
    the current route when creating a missing queue.
    
    This is useful when using custom routing class (specified by
    CELERY_ROUTES) which decides a destination at runtime.
    
    Closes #577

--- celery/routes.py
+++ celery/routes.py
@@ -67,7 +67,11 @@ class Router(object):
                 if not self.create_missing:
                     raise QueueNotFound(
                         "Queue %r is not defined in CELERY_QUEUES" % queue)
-                dest = dict(self.app.amqp.queues.add(queue, queue, queue))
+                if route.get("exchange") is None:
+                    route["exchange"] = queue
+                if route.get("routing_key") is None:
+                    route["routing_key"] = queue
+                dest = dict(self.app.amqp.queues.add(queue, **route))
             # needs to be declared by publisher
             dest["queue"] = queue
             # routing_key and binding_key are synonyms.
