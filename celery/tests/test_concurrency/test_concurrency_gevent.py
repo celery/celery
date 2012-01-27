b37075c5294341080aae3f535231296d6142f787
mlavin <markdlavin@gmail.com>
2012-01-26 21:07:34 -0500

    Test grow/shrink by n rather than default (n=1).

--- celery/tests/test_concurrency/test_concurrency_gevent.py
+++ celery/tests/test_concurrency/test_concurrency_gevent.py
@@ -25,12 +25,26 @@ class test_TaskPool(GeventCase):
         pool.grow()
         self.assertEqual(pool._pool.size, 11)
 
+    def test_grow_many(self):
+        pool = TaskPool(10)
+        pool.start()
+        self.assertEqual(pool._pool.size, 10)
+        pool.grow(2)
+        self.assertEqual(pool._pool.size, 12)
+
     def test_shrink(self):
         pool = TaskPool(10)
         pool.start()
         self.assertEqual(pool._pool.size, 10)
         pool.shrink()
         self.assertEqual(pool._pool.size, 9)
+
+    def test_shrink_many(self):
+        pool = TaskPool(10)
+        pool.start()
+        self.assertEqual(pool._pool.size, 10)
+        pool.shrink(2)
+        self.assertEqual(pool._pool.size, 8)
 
     def test_num_processes(self):
         pool = TaskPool(10)
