ec78cf75fa468ad3adbb8ba86cf388d0aa071853
Grant Thomas <jgrantthomas@gmail.com>
2012-03-21 15:12:16 -0700

    Fixes to allow for chord callbacks in always_eager mode

--- celery/task/chords.py
+++ celery/task/chords.py
@@ -13,7 +13,7 @@ from __future__ import absolute_import
 
 from .. import current_app
 from ..utils import uuid
-
+from ..task.sets import subtask
 
 class chord(object):
     Chord = None
@@ -25,6 +25,9 @@ class chord(object):
 
     def __call__(self, body, **options):
         tid = body.options.setdefault("task_id", uuid())
-        self.Chord.apply_async((list(self.tasks), body), self.options,
+        taskset_result = self.Chord.apply_async((list(self.tasks), body), self.options,
                                 **options)
+        if self.Chord.app.conf.CELERY_ALWAYS_EAGER:
+            return subtask(body).apply(args=(taskset_result.result.join(),))
+            
         return body.type.AsyncResult(tid)
