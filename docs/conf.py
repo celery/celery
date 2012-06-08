51a35ec446d1e722c7a8624fc82d657d8e7a350d
Ask Solem <ask@celeryproject.org>
2012-06-08 15:53:57 +0100

    Adds some misconceptions to FAQ

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,44 @@ html_sidebars = {
 
 ### Issuetracker
 
-if False:
+if True:
     issuetracker = "github"
     issuetracker_project = "celery/celery"
     issuetracker_issue_pattern = r'[Ii]ssue #(\d+)'
+
+# -- Options for Epub output ---------------------------------------------------
+
+# Bibliographic Dublin Core info.
+epub_title = 'Celery Manual, Version 2.6'
+epub_author = 'Ask Solem'
+epub_publisher = 'Celery Project'
+epub_copyright = '2009-2012'
+
+# The language of the text. It defaults to the language option
+# or en if the language is not set.
+epub_language = 'en'
+
+# The scheme of the identifier. Typical schemes are ISBN or URL.
+epub_scheme = 'ISBN'
+
+# The unique identifier of the text. This can be a ISBN number
+# or the project homepage.
+epub_identifier = 'celeryproject.org'
+
+# A unique identification for the text.
+epub_uid = 'Celery Manual, Version 2.6'
+
+# HTML files that should be inserted before the pages created by sphinx.
+# The format is a list of tuples containing the path and title.
+#epub_pre_files = []
+
+# HTML files shat should be inserted after the pages created by sphinx.
+# The format is a list of tuples containing the path and title.
+#epub_post_files = []
+
+# A list of files that should not be packed into the epub file.
+epub_exclude_files = ['search.html']
+
+
+# The depth of the table of contents in toc.ncx.
+epub_tocdepth = 3
--- docs/index.rst
+++ docs/index.rst
@@ -2,6 +2,18 @@
  Celery - Distributed Task Queue
 =================================
 
+Celery aims to be a fast, reliable and flexible, best-of-breed solution
+to process vast amounts of messages in a distributed fashion, while
+providing operations with the tools to maintain such a system.
+
+Celery is Open Source and licensed under the `BSD License`_.
+
+If you're new to Celery you can get started by following
+the :ref:`first-steps` tutorial.
+
+.. _`BSD License`: http://www.opensource.org/licenses/BSD-3-Clause
+
+
 Contents:
 
 .. toctree::
