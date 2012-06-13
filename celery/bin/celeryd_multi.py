e1208c78f070824552d2a449917f88d50a58cf5c
Loren Abrams <labrams@hearsaycorp.com>
2012-06-13 19:19:41 +0000

    honor the newline parameter

--- celery/bin/celeryd_multi.py
+++ celery/bin/celeryd_multi.py
@@ -185,7 +185,7 @@ class MultiTool(object):
         return self.retcode
 
     def say(self, m, newline=True):
-        self.fh.write("%s\n" % m)
+        self.fh.write("%s%s" % (m, '\n' if newline else ''))
 
     def names(self, argv, cmd):
         p = NamespacedOptionParser(argv)
