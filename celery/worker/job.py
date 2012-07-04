5585d3a9991c2d4fc31cf8ec84b01e0936cbd091
David Markey <dmarkey@localhost.localdomain>
2012-07-04 23:21:38 +0100

    When ACKS_LATE is enabled, make sure the message is ACK'd on a retry

--- celery/worker/job.py
+++ celery/worker/job.py
@@ -321,6 +321,9 @@ class Request(object):
                          exception=safe_repr(exc_info.exception.exc),
                          traceback=safe_str(exc_info.traceback))
 
+        if self.task.acks_late:
+            self.acknowledge()
+
         if _does_info:
             info(self.retry_msg.strip(), {
                 'id': self.id, 'name': self.name,
