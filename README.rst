2e5a84930e61d148260229bbfeb62d1123e30280
Ask Solem <ask@celeryproject.org>
2012-07-07 14:40:51 +0100

    master is now 3.1.0a1

--- README.rst
+++ README.rst
@@ -4,7 +4,7 @@
 
 .. image:: http://cloud.github.com/downloads/celery/celery/celery_128.png
 
-:Version: 3.0.0 (Chiastic Slide)
+:Version: 3.1.0a1 (DEVEL)
 :Web: http://celeryproject.org/
 :Download: http://pypi.python.org/pypi/celery/
 :Source: http://github.com/celery/celery/
--- celery/__init__.py
+++ celery/__init__.py
@@ -7,8 +7,8 @@
 
 from __future__ import absolute_import
 
-SERIES = 'Chiastic Slide'
-VERSION = (3, 0, 0)
+SERIES = 'DEVEL'
+VERSION = (3, 1, 0, 'a1')
 __version__ = '.'.join(map(str, VERSION[0:3])) + ''.join(VERSION[3:])
 __author__ = 'Ask Solem'
 __contact__ = 'ask@celeryproject.org'
