a86f32440022c1a4c8376d0dc05cadfc17ede5fb
Ask Solem <ask@celeryproject.org>
2012-07-30 17:49:59 +0100

    celery.tests.config: Remove TT_ + CELERY_REDIS_ config

--- celery/tests/config.py
+++ celery/tests/config.py
@@ -28,16 +28,6 @@ CELERY_ENABLE_UTC = True
 
 CELERYD_LOG_COLOR = False
 
-# Tyrant results tests (only executed if installed and running)
-TT_HOST = os.environ.get('TT_HOST') or 'localhost'
-TT_PORT = int(os.environ.get('TT_PORT') or 1978)
-
-# Redis results tests (only executed if installed and running)
-CELERY_REDIS_HOST = os.environ.get('REDIS_HOST') or 'localhost'
-CELERY_REDIS_PORT = int(os.environ.get('REDIS_PORT') or 6379)
-CELERY_REDIS_DB = os.environ.get('REDIS_DB') or 0
-CELERY_REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
-
 # Mongo results tests (only executed if installed and running)
 CELERY_MONGODB_BACKEND_SETTINGS = {
     'host': os.environ.get('MONGO_HOST') or 'localhost',
