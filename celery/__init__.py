d89d88efc56ed9a8e015f8eb152e9fcfbae103e1
Ask Solem <ask@celeryproject.org>
2012-08-01 16:23:51 +0100

    Bumps version to 3.0.5 and updates Changelog

--- celery/__init__.py
+++ celery/__init__.py
@@ -8,7 +8,7 @@
 from __future__ import absolute_import
 
 SERIES = 'Chiastic Slide'
-VERSION = (3, 0, 4)
+VERSION = (3, 0, 5)
 __version__ = '.'.join(map(str, VERSION[0:3])) + ''.join(VERSION[3:])
 __author__ = 'Ask Solem'
 __contact__ = 'ask@celeryproject.org'
