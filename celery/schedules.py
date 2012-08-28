6fd700f84377b8f64a16de7ee9eae00f63dafe7f
Andrii Kostenko <andrey@kostenko.name>
2012-08-29 02:40:43 +0400

    Fix a bug with incorrect remaining date
    If date is today and hour is in the past, celerybeat goes crazy

--- celery/schedules.py
+++ celery/schedules.py
@@ -436,7 +436,8 @@ class crontab(schedule):
 
         execute_this_date = (last_run_at.month in self.month_of_year and
                                 last_run_at.day in self.day_of_month and
-                                    dow_num in self.day_of_week)
+                                    dow_num in self.day_of_week
+                                    and last_run_at.hour < max(self.hour))
 
         execute_this_hour = (execute_this_date and
                                 last_run_at.hour in self.hour and
