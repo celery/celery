ce1d8d5d8c6e2b8d8a296d7bf39dcc672be9d5dc
Ask Solem <ask@celeryproject.org>
2013-05-14 14:32:05 +0100

    Fix for celery.utils.threads.Event. Re #1354

--- celery/utils/threads.py
+++ celery/utils/threads.py
@@ -26,22 +26,18 @@ active_count = (getattr(threading, 'active_count', None) or
                 threading.activeCount)
 
 
-class Event(_Event):
+if sys.version_info < (2, 6):
 
-    if not hasattr(_Event, 'is_set'):     # pragma: no cover
+    class Event(_Event):                  # pragma: no cover
         is_set = _Event.isSet
 
-
-class Thread(_Thread):
-
-    if not hasattr(_Thread, 'is_alive'):  # pragma: no cover
+    class Thread(_Thread):                # pragma: no cover
         is_alive = _Thread.isAlive
-
-    if not hasattr(_Thread, 'daemon'):    # pragma: no cover
         daemon = property(_Thread.isDaemon, _Thread.setDaemon)
-
-    if not hasattr(_Thread, 'name'):      # pragma: no cover
         name = property(_Thread.getName, _Thread.setName)
+else:
+    Event = _Event
+    Thread = _Thread
 
 
 class bgThread(Thread):
