f6bef0230a77d731564eb2091ce0b5ca5e69f781
Ask Solem <ask@celeryproject.org>
2013-09-23 18:16:10 +0100

    Mingle/Gossip sync with originator

--- celery/worker/control.py
+++ celery/worker/control.py
@@ -246,9 +246,13 @@ def dump_revoked(state, **kwargs):
 
 
 @Panel.register
-def hello(state, **kwargs):
-    return {'revoked': worker_state.revoked._data,
-            'clock': state.app.clock.forward()}
+def hello(state, from_node, revoked=None, **kwargs):
+    if from_node != state.hostname:
+        logger.info('sync with %s', from_node)
+        worker_state.revoked.update(revoked)
+        return  {'revoked': worker_state.revoked._data,
+                 'clock': state.app.clock.forward()}
+
 
 
 @Panel.register
