8247879d5c35288354ceddd27f26bca94e7ee3cd
Matt Wise <matt@nextdoor.com>
2013-11-06 09:30:42 -0800

    Provide BROKER_FAILOVER_STRATEGY option in Celery.
    This option allows for either selecting pre-existing kombu.connection
    failover strategies, or passing in your own method reference.

--- celery/app/base.py
+++ celery/app/base.py
@@ -258,7 +258,8 @@ class Celery(object):
     def connection(self, hostname=None, userid=None,
                    password=None, virtual_host=None, port=None, ssl=None,
                    insist=None, connect_timeout=None, transport=None,
-                   transport_options=None, heartbeat=None, **kwargs):
+                   transport_options=None, heartbeat=None,
+                   failover_strategy=None, **kwargs):
         conf = self.conf
         return self.amqp.Connection(
             hostname or conf.BROKER_HOST,
@@ -272,6 +273,7 @@ class Celery(object):
             connect_timeout=self.either(
                 'BROKER_CONNECTION_TIMEOUT', connect_timeout),
             heartbeat=heartbeat,
+            failover_strategy=failover_strategy or conf.BROKER_FAILOVER_STRATEGY,
             transport_options=dict(conf.BROKER_TRANSPORT_OPTIONS,
                                    **transport_options or {}))
     broker_connection = connection
