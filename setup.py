d6ac431602066db3b79b696c2b53e5a76a404e2c
Ask Solem <ask@celeryproject.org>
2013-11-09 23:01:29 +0000

    Fix release tool errors

--- setup.py
+++ setup.py
@@ -71,7 +71,6 @@ classes = """
     Programming Language :: Python :: 2.7
     Programming Language :: Python :: 3
     Programming Language :: Python :: 3.3
-    Programming Language :: Python :: 3.4
     Programming Language :: Python :: Implementation :: CPython
     Programming Language :: Python :: Implementation :: PyPy
     Programming Language :: Python :: Implementation :: Jython
