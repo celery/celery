51e333539f1e43327c5809d2f3fee1a7a1c0fc28
Ask Solem <ask@celeryproject.org>
2014-01-13 18:41:45 +0000

    Docs should add .. to path so that the current version of celery is always used.  Closes #1782

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ this = os.path.dirname(os.path.abspath(__file__))
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
-sys.path.append(os.path.join(os.pardir, 'tests'))
+sys.path.insert(0, os.path.join(this, os.pardir))
 sys.path.append(os.path.join(this, '_ext'))
 import celery
 
