celery/celery

commit 022ee4ade9f7d03f995413212b135fe18451be95

[Py3] Fixes unordrable problem in close_open_fds

celery/platforms.py
@@ -244,9 +244,9 @@ def _create_pidlock(pidfile):
 if hasattr(os, 'closerange'):
 
     def close_open_fds(keep=None):
-        keep = [maybe_fileno(f)
-                for f in uniq(sorted(keep or []))
-                if maybe_fileno(f) is not None]
+        keep = list(uniq(sorted(filter(None, (
+            maybe_fileno(f) for f in keep or []
+        )))))
         maxfd = get_fdmax(default=2048)
         kL, kH = iter([-1] + keep), iter(keep + [maxfd])
         for low, high in zip_longest(kL, kH):

commit 22885960bb8cfd1b6f9f23058fe08768a22cb6aa

Fix Celerybeat from hanging when using --detach mode after dispatching 1st set of tasks.
The change made in https://github.com/celery/celery/commit/022ee4ade9f7d03f995413212b135fe18451be95
caused file descriptor 0 (usually sys.stdin) not to be included in the list of file descriptors to keep.
Doing a filter(none) apparently removes this number from this list, which somehow causes sys.stdin to be closed.
Changed the logic to be filter(lambda: x is not None) to allow 0 to be included.

See https://github.com/celery/celery/pull/1822.

celery/platforms.py
@@ -266,7 +266,8 @@ def _create_pidlock(pidfile):
 if hasattr(os, 'closerange'):
 
     def close_open_fds(keep=None):
-        keep = list(uniq(sorted(filter(None, (
+        # using filter(None) causes 0 to be filtered out too
+        keep = list(uniq(sorted(filter(lambda x: x is not None, (
             maybe_fileno(f) for f in keep or []
         )))))
         maxfd = get_fdmax(default=2048)