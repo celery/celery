cbb9cfc7642e937325617c58972c4af60cec7130
Ask Solem <ask@celeryproject.org>
2014-02-04 17:12:48 +0000

    [3.0] Make sure docs can be built

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@ this = os.path.dirname(os.path.abspath(__file__))
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
+sys.path.insert(0, os.pardir)
 sys.path.append(os.path.join(os.pardir, "tests"))
 sys.path.append(os.path.join(this, "_ext"))
 import celery
