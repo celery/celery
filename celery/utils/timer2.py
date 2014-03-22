c353c804f12efa3bd861bbc96e3972bc26be5e49
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-03-23 00:39:34 +0200

    Fix issue with timer starting after stop() is called. Now it doesn't start in this situation.

--- celery/utils/timer2.py
+++ celery/utils/timer2.py
@@ -86,8 +86,8 @@ class Timer(threading.Thread):
             os._exit(1)
 
     def stop(self):
+        self._is_shutdown.set()
         if self.running:
-            self._is_shutdown.set()
             self._is_stopped.wait()
             self.join(THREAD_TIMEOUT_MAX)
             self.running = False
