3cbe86c9e5379e77daa9cc20bf55016a47369709
Matthew Duggan <mgithub@guarana.org>
2014-04-09 14:09:29 +0900

    Add option to worker to control heartbeat interval.
    Thanks to my colleague Craig Northway for the patch.

--- celery/worker/consumer.py
+++ celery/worker/consumer.py
@@ -534,12 +534,15 @@ class Events(bootsteps.StartStopStep):
 class Heart(bootsteps.StartStopStep):
     requires = (Events, )
 
-    def __init__(self, c, without_heartbeat=False, **kwargs):
+    def __init__(self, c, without_heartbeat=False, heartbeat_interval=None,
+                 **kwargs):
         self.enabled = not without_heartbeat
+        self.heartbeat_interval = heartbeat_interval
         c.heart = None
 
     def start(self, c):
-        c.heart = heartbeat.Heart(c.timer, c.event_dispatcher)
+        c.heart = heartbeat.Heart(c.timer, c.event_dispatcher,
+            self.heartbeat_interval)
         c.heart.start()
 
     def stop(self, c):
