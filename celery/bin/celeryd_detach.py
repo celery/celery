2dcc8de08ef8576d7dc924a43608410c213cd3c0
Ask Solem <ask@celeryproject.org>
2014-04-25 14:02:07 +0100

    Worker --detach must forward working_directory option.  Closes #2003

--- celery/bin/celeryd_detach.py
+++ celery/bin/celeryd_detach.py
@@ -30,6 +30,7 @@ logger = get_logger(__name__)
 C_FAKEFORK = os.environ.get('C_FAKEFORK')
 
 OPTION_LIST = daemon_options(default_pidfile='celeryd.pid') + (
+    Option('--workdir', default='/', dest='working_directory'),
     Option('--fake',
            default=False, action='store_true', dest='fake',
            help="Don't fork (for debugging purposes)"),
