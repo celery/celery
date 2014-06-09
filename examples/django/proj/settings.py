f8027b961b6cbd97eb55c1165fd5501605100e47
Ask Solem <ask@celeryproject.org>
2014-06-09 15:35:52 +0100

    Django example; Use json serializers

--- examples/django/proj/settings.py
+++ examples/django/proj/settings.py
@@ -11,6 +11,8 @@ BROKER_URL = 'amqp://guest:guest@localhost//'
 #: Only add pickle to this list if your broker is secured
 #: from unwanted access (see userguide/security.html)
 CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 
 # Django settings for proj project.
 
