celery/celery

commit 6d64d7a24f15311a0bd13baf7f6a9879176d2d08

amqp/cassandra/mongodb backends now set result.children

celery/backends/cassandra.py
@@ -131,7 +131,8 @@ class CassandraBackend(BaseDictBackend):
             date_done = self.app.now()
             meta = {"status": status,
                     "date_done": date_done.strftime('%Y-%m-%dT%H:%M:%SZ'),
-                    "traceback": self.encode(traceback)}
+                    "traceback": self.encode(traceback),
+                    "children": self.encode(self.current_task_children())}
             if self.detailed_mode:
                 meta["result"] = result
                 cf.insert(task_id, {date_done: self.encode(meta)},
@@ -161,6 +162,7 @@ class CassandraBackend(BaseDictBackend):
                         "result": self.decode(obj["result"]),
                         "date_done": obj["date_done"],
                         "traceback": self.decode(obj["traceback"]),
+                        "children": self.decode(obj["children"]),
                     }
             except (KeyError, pycassa.NotFoundException):
                 meta = {"status": states.PENDING, "result": None}

celery/backends/amqp.py
@@ -118,7 +118,8 @@ class AMQPBackend(BaseDictBackend):
                             interval_max=interval_max)
                 send(conn, task_id, {"task_id": task_id, "status": status,
                                 "result": self.encode_result(result, status),
-                                "traceback": traceback})
+                                "traceback": traceback,
+                                "children": self.current_task_children()})
         return result
 
     def get_task_meta(self, task_id, cache=True):

celery/backends/mongodb.py
@@ -107,7 +107,8 @@ class MongoBackend(BaseDictBackend):
                 "status": status,
                 "result": Binary(self.encode(result)),
                 "date_done": datetime.utcnow(),
-                "traceback": Binary(self.encode(traceback))}
+                "traceback": Binary(self.encode(traceback)),
+                "children": Binary(self.encode(self.current_task_children()))}
         self.collection.save(meta, safe=True)
 
         return result
@@ -125,6 +126,7 @@ class MongoBackend(BaseDictBackend):
             "result": self.decode(obj["result"]),
             "date_done": obj["date_done"],
             "traceback": self.decode(obj["traceback"]),
+            "children": self.decode(obj["children"]),
         }
 
         return meta

celery/bin/celeryd.py
@@ -12,7 +12,7 @@
     Number of child processes processing the queue. The default
     is the number of CPUs available on your system.
 
-.. cmdoption:: -c, --pool
+.. cmdoption:: -P, --pool
 
     Pool implementation:
 

commit 4192b368770d0f1d6ea21ad7492ed4cca94d7f0f

Fix results for cassandra backend in detailed mode
When using the Cassandra backend in detailed mode, attempting to
retrieve the results results in an error because of improperly
decoded metadata (introduced by
6d64d7a24f15311a0bd13baf7f6a9879176d2d08).

The metadata for `children` and `traceback` are encoded before being
grouped into the dictionary, which itself is encoded later prior to
being stored in Cassandra. During retrieval in detailed mode, these
are not decoded again, causing them to be interpreted as encoded
unicode literals instead of their native types.

CHANGES:
- the `results` metadata is encoded regardless of whether Cassandra is
  used in detailed or non-detailed mode. This is being consistent with
  the other backends where the results are always encoded. It may be
  unnecessary (results in double encoding), however.

- The logic for encoding/decoding metadata is now consistent for both
  detailed and non-detailed modes.

REFERENCE:
- 21cdf82fc4310320c21687cf80a3f98802565bd5 - cassandra detailed mode
- 6d64d7a24f15311a0bd13baf7f6a9879176d2d08 - backends support children

celery/backends/cassandra.py
@@ -145,15 +145,14 @@ class CassandraBackend(BaseBackend):
             meta = {'status': status,
                     'date_done': date_done.strftime('%Y-%m-%dT%H:%M:%SZ'),
                     'traceback': self.encode(traceback),
+                    'result': self.encode(result),
                     'children': self.encode(
                         self.current_task_children(request),
                     )}
             ttl = self.expires and max(self.expires.total_seconds(), 0)
             if self.detailed_mode:
-                meta['result'] = result
                 cf.insert(task_id, {date_done: self.encode(meta)}, ttl=ttl)
             else:
-                meta['result'] = self.encode(result)
                 cf.insert(task_id, meta, ttl=ttl)
 
         return self._retry_on_error(_do_store)
@@ -166,18 +165,18 @@ class CassandraBackend(BaseBackend):
             try:
                 if self.detailed_mode:
                     row = cf.get(task_id, column_reversed=True, column_count=1)
-                    meta = self.decode(list(row.values())[0])
-                    meta['task_id'] = task_id
+                    obj = self.decode(list(row.values())[0])
                 else:
                     obj = cf.get(task_id)
-                    meta = {
-                        'task_id': task_id,
-                        'status': obj['status'],
-                        'result': self.decode(obj['result']),
-                        'date_done': obj['date_done'],
-                        'traceback': self.decode(obj['traceback']),
-                        'children': self.decode(obj['children']),
-                    }
+
+                meta = {
+                    'task_id': task_id,
+                    'status': obj['status'],
+                    'result': self.decode(obj['result']),
+                    'date_done': obj['date_done'],
+                    'traceback': self.decode(obj['traceback']),
+                    'children': self.decode(obj['children']),
+                }
             except (KeyError, pycassa.NotFoundException):
                 meta = {'status': states.PENDING, 'result': None}
             return meta