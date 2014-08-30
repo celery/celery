be0b620411a6adb7d05955d5f7411d16a01263d7
Aneil Mallavarapu <aneil.mallavar@gmail.com>
2014-08-30 13:32:01 -0700

    Fix issue #2225
    Creating a chord no longer results in "TypeError: group object got
    multiple values for keyword argument 'task_id'".  Chords now
    complete without hanging.

--- celery/backends/base.py
+++ celery/backends/base.py
@@ -519,7 +519,11 @@ class KeyValueStoreBackend(BaseBackend):
     def _apply_chord_incr(self, header, partial_args, group_id, body,
                           result=None, options={}, **kwargs):
         self.save_group(group_id, self.app.GroupResult(group_id, result))
-        return header(*partial_args, task_id=group_id, **options or {})
+
+        fixed_options = dict((k,v) for k,v in options.items() if k != 'task_id')
+
+        return header(*partial_args, task_id=group_id, **fixed_options or {})
+
 
     def on_chord_part_return(self, task, state, result, propagate=None):
         if not self.implements_incr:
