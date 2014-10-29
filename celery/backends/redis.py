a46d337c2cce82c7742f13c2c1349b6a8b5321af
Ask Solem <ask@celeryproject.org>
2014-10-29 17:28:56 +0000

    Clone must keep chord_size for all signature types.  Closes #2339

--- celery/backends/redis.py
+++ celery/backends/redis.py
@@ -180,8 +180,11 @@ class RedisBackend(KeyValueStoreBackend):
         self.client.incr(self.get_key_for_group(group_id, '.t'), 1)
 
     def _unpack_chord_result(self, tup, decode,
+                             EXCEPTION_STATES=states.EXCEPTION_STATES,
                              PROPAGATE_STATES=states.PROPAGATE_STATES):
         _, tid, state, retval = decode(tup)
+        if state in EXCEPTION_STATES:
+            retval = self.exception_to_python(retval)
         if state in PROPAGATE_STATES:
             raise ChordError('Dependency {0} raised {1!r}'.format(tid, retval))
         return retval
@@ -220,7 +223,7 @@ class RedisBackend(KeyValueStoreBackend):
             callback = maybe_signature(request.chord, app=app)
             total = callback['chord_size'] + totaldiff
             if readycount == total:
-                decode, unpack = self.decode_result, self._unpack_chord_result
+                decode, unpack = self.decode, self._unpack_chord_result
                 resl, _, _ = client.pipeline()  \
                     .lrange(jkey, 0, total)     \
                     .delete(jkey)               \
