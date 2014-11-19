c67b0eaf11c4e0e8dbff8073d283d0650d029db7
bee-keeper <ricbottomley@gmail.com>
2014-11-19 14:20:39 +0000

    removed underscore

--- extra/supervisord/celery.sh
+++ extra/supervisord/celery.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source {{ additional variables }}
-exec celery --app={{ application_name }}._celery:app worker --loglevel=INFO -n worker.%%h
+exec celery --app={{ application_name }}.celery:app worker --loglevel=INFO -n worker.%%h
