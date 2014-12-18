81842050e6f08d05afcf3f61739b7cb06e75b566
Ask Solem <ask@celeryproject.org>
2014-12-18 15:02:11 +0000

    Tests passing

--- celery/tests/bin/test_celeryd_detach.py
+++ celery/tests/bin/test_celeryd_detach.py
@@ -85,7 +85,7 @@ class test_Command(AppCase):
         detach.assert_called_with(
             path=x.execv_path, uid=None, gid=None,
             umask=None, fake=False, logfile='/var/log', pidfile='celeryd.pid',
-            working_directory=None,
+            working_directory=None, executable=None,
             argv=x.execv_argv + [
                 '-c', '1', '-lDEBUG',
                 '--logfile=/var/log', '--pidfile=celeryd.pid',
