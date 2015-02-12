5392550384986e4232a4d9f3d0170bfb45ea3b29
Ask Solem <ask@celeryproject.org>
2015-02-12 21:23:58 +0000

    2015

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Celery'
-copyright = '2009-2014, Ask Solem & Contributors'
+copyright = '2009-2015, Ask Solem & Contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
