c5247872a7febd543060febcb29875be051ba350
Tristan Carel <tristan@cogniteev.com>
2015-03-11 13:33:33 +0100

    Fix redis tests

--- celery/backends/redis.py
+++ celery/backends/redis.py
@@ -98,12 +98,13 @@ class RedisBackend(KeyValueStoreBackend):
 
         self.connparams['sentinel'] = strtobool(self.connparams.get('sentinel', False))
         # resolve default port
-        if 'port' not in self.connparams:
-            self.connparams['port'] = _get('DB') or (\
-                RedisBackend.SENTINEL_DEFAULT_PORT \
-                if self.connparams['sentinel']
-                else RedisBackend.REDIS_DEFAULT_PORT
-            )
+        if not self.connparams.get('path', '').startswith('/'): # no socket connection
+            if 'port' not in self.connparams:
+                self.connparams['port'] = _get('DB') or (\
+                    RedisBackend.SENTINEL_DEFAULT_PORT \
+                    if self.connparams['sentinel']
+                    else RedisBackend.REDIS_DEFAULT_PORT
+                )
 
         if not self.connparams['sentinel']:
             self.connparams.pop('sentinel')
