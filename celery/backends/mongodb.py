772bcb832b91bdc361beaf3bb775a9f4a481faab
fatihsucu <fatihsucu0@gmail.com>
2015-04-16 14:31:59 +0300

    mongodb default options removed.

--- celery/backends/mongodb.py
+++ celery/backends/mongodb.py
@@ -78,9 +78,6 @@ class MongoBackend(BaseBackend):
 
         self.url = url
 
-        # default options
-        self.options.setdefault('max_pool_size', self.max_pool_size)
-        self.options.setdefault('auto_start_request', False)
 
         # update conf with mongo uri data, only if uri was given
         if self.url:
