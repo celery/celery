ac1c655f43d85d531a57d2e1313a8141608409a2
Michael Duane Mooring <mikeumus@gmail.com>
2015-06-20 16:25:11 -0400

    django.contrib.admin is already uncommented above
    And I don't think the order or where `django.contrib.admin` is placed is of consequence.

--- examples/django/proj/settings.py
+++ examples/django/proj/settings.py
@@ -134,8 +134,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'kombu.transport.django.KombuAppConfig',
     'demoapp',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
