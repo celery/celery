b242f1bffa839e27f53677720cca81174637e0f7
Omer Katz <omer.drow@gmail.com>
2015-07-14 10:25:31 +0300

    Use dict comprehension instead of trasforming a generator into a dict.

--- celery/backends/base.py
+++ celery/backends/base.py
@@ -357,7 +357,7 @@ class BaseBackend(object):
 
     def apply_chord(self, header, partial_args, group_id, body,
                     options={}, **kwargs):
-        fixed_options = dict((k,v) for k,v in options.items() if k!='task_id')
+        fixed_options = {k: v for k,v in options.items() if k!='task_id'}
         result = header(*partial_args, task_id=group_id, **fixed_options or {})
         self.fallback_chord_unlock(group_id, body, **kwargs)
         return result
