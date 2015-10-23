0c8c7df4e3bb0ad51235067cd4fde1a5c8746b89
Ask Solem <ask@celeryproject.org>
2015-10-22 18:41:46 -0700

    OpenSSL tests works on Python 3 again.  Closes #2040

--- celery/tests/security/case.py
+++ celery/tests/security/case.py
@@ -8,8 +8,6 @@ import sys
 class SecurityCase(AppCase):
 
     def setup(self):
-        if sys.version_info[0] == 3:
-            raise SkipTest('PyOpenSSL does not work on Python 3')
         try:
             from OpenSSL import crypto  # noqa
         except ImportError:
