730e00adf2cde3e735215e22d5e1a18149691238
Ask Solem <ask@celeryproject.org>
2015-10-22 18:37:07 -0700

    Adds Trove classifier for Python 3.5

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ classes = """
     Programming Language :: Python :: 3
     Programming Language :: Python :: 3.3
     Programming Language :: Python :: 3.4
+    Programming Language :: Python :: 3.5
     Programming Language :: Python :: Implementation :: CPython
     Programming Language :: Python :: Implementation :: PyPy
     Programming Language :: Python :: Implementation :: Jython
