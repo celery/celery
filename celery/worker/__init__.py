d80ad64dceaf443c4168593866b6b4de95c0aab3
Ask Solem <ask@celeryproject.org>
2015-12-10 15:21:11 -0800

    WorkController.__repr__ failed if not fully setup. Closes #2514

--- celery/worker/__init__.py
+++ celery/worker/__init__.py
@@ -332,7 +332,8 @@ class WorkController(object):
 
     def __repr__(self):
         return '<Worker: {self.hostname} ({state})>'.format(
-            self=self, state=self.blueprint.human_state(),
+            self=self,
+            state=self.blueprint.human_state() if self.blueprint else 'INIT',
         )
 
     def __str__(self):
