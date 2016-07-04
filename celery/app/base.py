781a81cf7e1912032bcaf87ecb83ee6a7fcdd837
Ask Solem <ask@celeryproject.org>
2016-07-04 14:11:40 -0700

    Removes app.default_connection() (use app.connection_or_acquire())

--- celery/app/base.py
+++ celery/app/base.py
@@ -761,7 +761,6 @@ class Celery:
                 will be acquired from the connection pool.
         """
         return FallbackContext(connection, self._acquire_connection, pool=pool)
-    default_connection = connection_or_acquire  # XXX compat
 
     def producer_or_acquire(self, producer=None):
         """For use within a :keyword:`with` statement to get a producer
