cabf153fe860a156f9a14e33ffae309a4c5dd1f0
Andrew Stewart <astewart@twistbioscience.com>
2016-07-04 22:22:58 -0700

    Deserialize json string prior to inclusion in CouchDB doc

--- celery/backends/couchdb.py
+++ celery/backends/couchdb.py
@@ -8,6 +8,8 @@
 """
 from __future__ import absolute_import, unicode_literals
 
+import json
+
 try:
     import pycouchdb
 except ImportError:
@@ -91,13 +93,16 @@ class CouchBackend(KeyValueStoreBackend):
             return None
 
     def set(self, key, value):
-        data = {'_id': key, 'value': value}
+        # data = {'_id': key, 'value': value}
+        data = json.loads(value)
+        data['_id'] = key
         try:
             self.connection.save(data)
         except pycouchdb.exceptions.Conflict:
             # document already exists, update it
             data = self.connection.get(key)
-            data['value'] = value
+            # data['value'] = value
+            data.update(json.loads(value))
             self.connection.save(data)
 
     def mget(self, keys):
