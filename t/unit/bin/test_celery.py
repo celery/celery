0919237b0d24753f4eacc3e48260f7e2878c51ae
Ask Solem <ask@celeryproject.org>
2016-10-31 13:34:02 -0700

    Commands: Each command now has separate module.

--- t/unit/bin/test_celery.py
+++ t/unit/bin/test_celery.py
@@ -3,31 +3,19 @@ from __future__ import absolute_import, unicode_literals
 import pytest
 import sys
 
-from datetime import datetime
-
 from case import Mock, patch
-from kombu.utils.json import dumps
 
 from celery import __main__
 from celery.bin.base import Error
 from celery.bin import celery as mod
 from celery.bin.celery import (
     Command,
-    list_,
-    call,
-    purge,
-    result,
-    inspect,
-    control,
-    status,
-    migrate,
     help,
     report,
     CeleryCommand,
     determine_exit_status,
     multi,
     main as mainfun,
-    _RemoteControl,
 )
 from celery.five import WhateverIO
 from celery.platforms import EX_FAILURE, EX_USAGE, EX_OK
@@ -105,144 +93,6 @@ class test_Command:
         assert 'OK' in str(self.cmd.pretty('the quick brown'))
         assert 'OK' in str(self.cmd.pretty(object()))
         assert 'OK' in str(self.cmd.pretty({'foo': 'bar'}))
-
-
-class test_list:
-
-    def test_list_bindings_no_support(self):
-        l = list_(app=self.app, stderr=WhateverIO())
-        management = Mock()
-        management.get_bindings.side_effect = NotImplementedError()
-        with pytest.raises(Error):
-            l.list_bindings(management)
-
-    def test_run(self):
-        l = list_(app=self.app, stderr=WhateverIO())
-        l.run('bindings')
-
-        with pytest.raises(Error):
-            l.run(None)
-
-        with pytest.raises(Error):
-            l.run('foo')
-
-
-class test_call:
-
-    def setup(self):
-
-        @self.app.task(shared=False)
-        def add(x, y):
-            return x + y
-        self.add = add
-
-    @patch('celery.app.base.Celery.send_task')
-    def test_run(self, send_task):
-        a = call(app=self.app, stderr=WhateverIO(), stdout=WhateverIO())
-        a.run(self.add.name)
-        send_task.assert_called()
-
-        a.run(self.add.name,
-              args=dumps([4, 4]),
-              kwargs=dumps({'x': 2, 'y': 2}))
-        assert send_task.call_args[1]['args'], [4 == 4]
-        assert send_task.call_args[1]['kwargs'] == {'x': 2, 'y': 2}
-
-        a.run(self.add.name, expires=10, countdown=10)
-        assert send_task.call_args[1]['expires'] == 10
-        assert send_task.call_args[1]['countdown'] == 10
-
-        now = datetime.now()
-        iso = now.isoformat()
-        a.run(self.add.name, expires=iso)
-        assert send_task.call_args[1]['expires'] == now
-        with pytest.raises(ValueError):
-            a.run(self.add.name, expires='foobaribazibar')
-
-
-class test_purge:
-
-    def test_run(self):
-        out = WhateverIO()
-        a = purge(app=self.app, stdout=out)
-        a._purge = Mock(name='_purge')
-        a._purge.return_value = 0
-        a.run(force=True)
-        assert 'No messages purged' in out.getvalue()
-
-        a._purge.return_value = 100
-        a.run(force=True)
-        assert '100 messages' in out.getvalue()
-
-        a.out = Mock(name='out')
-        a.ask = Mock(name='ask')
-        a.run(force=False)
-        a.ask.assert_called_with(a.warn_prompt, ('yes', 'no'), 'no')
-        a.ask.return_value = 'yes'
-        a.run(force=False)
-
-
-class test_result:
-
-    def setup(self):
-
-        @self.app.task(shared=False)
-        def add(x, y):
-            return x + y
-        self.add = add
-
-    def test_run(self):
-        with patch('celery.result.AsyncResult.get') as get:
-            out = WhateverIO()
-            r = result(app=self.app, stdout=out)
-            get.return_value = 'Jerry'
-            r.run('id')
-            assert 'Jerry' in out.getvalue()
-
-            get.return_value = 'Elaine'
-            r.run('id', task=self.add.name)
-            assert 'Elaine' in out.getvalue()
-
-            with patch('celery.result.AsyncResult.traceback') as tb:
-                r.run('id', task=self.add.name, traceback=True)
-                assert str(tb) in out.getvalue()
-
-
-class test_status:
-
-    @patch('celery.bin.celery.inspect')
-    def test_run(self, inspect_):
-        out, err = WhateverIO(), WhateverIO()
-        ins = inspect_.return_value = Mock()
-        ins.run.return_value = []
-        s = status(self.app, stdout=out, stderr=err)
-        with pytest.raises(Error):
-            s.run()
-
-        ins.run.return_value = ['a', 'b', 'c']
-        s.run()
-        assert '3 nodes online' in out.getvalue()
-        s.run(quiet=True)
-
-
-class test_migrate:
-
-    @patch('celery.contrib.migrate.migrate_tasks')
-    def test_run(self, migrate_tasks):
-        out = WhateverIO()
-        m = migrate(app=self.app, stdout=out, stderr=WhateverIO())
-        with pytest.raises(TypeError):
-            m.run()
-        migrate_tasks.assert_not_called()
-
-        m.run('memory://foo', 'memory://bar')
-        migrate_tasks.assert_called()
-
-        state = Mock()
-        state.count = 10
-        state.strtotal = 30
-        m.on_migrate_task(state, {'task': 'tasks.add', 'id': 'ID'}, None)
-        assert '10/30' in out.getvalue()
 
 
 class test_report:
@@ -404,108 +254,6 @@ class test_CeleryCommand:
         with patch.dict(sys.modules, __main__=main):
             assert x.prepare_prog_name('__main__.py') == '/opt/foo.py'
             assert x.prepare_prog_name('celery') == 'celery'
-
-
-class test_RemoteControl:
-
-    def test_call_interface(self):
-        with pytest.raises(NotImplementedError):
-            _RemoteControl(app=self.app).call()
-
-
-class test_inspect:
-
-    def test_usage(self):
-        assert inspect(app=self.app).usage('foo')
-
-    def test_command_info(self):
-        i = inspect(app=self.app)
-        assert i.get_command_info(
-            'ping', help=True, color=i.colored.red, app=self.app,
-        )
-
-    def test_list_commands_color(self):
-        i = inspect(app=self.app)
-        assert i.list_commands(help=True, color=i.colored.red, app=self.app)
-        assert i.list_commands(help=False, color=None, app=self.app)
-
-    def test_epilog(self):
-        assert inspect(app=self.app).epilog
-
-    def test_do_call_method_sql_transport_type(self):
-        self.app.connection = Mock()
-        conn = self.app.connection.return_value = Mock(name='Connection')
-        conn.transport.driver_type = 'sql'
-        i = inspect(app=self.app)
-        with pytest.raises(i.Error):
-            i.do_call_method(['ping'])
-
-    def test_say_directions(self):
-        i = inspect(self.app)
-        i.out = Mock()
-        i.quiet = True
-        i.say_chat('<-', 'hello out')
-        i.out.assert_not_called()
-
-        i.say_chat('->', 'hello in')
-        i.out.assert_called()
-
-        i.quiet = False
-        i.out.reset_mock()
-        i.say_chat('<-', 'hello out', 'body')
-        i.out.assert_called()
-
-    @patch('celery.app.control.Control.inspect')
-    def test_run(self, real):
-        out = WhateverIO()
-        i = inspect(app=self.app, stdout=out)
-        with pytest.raises(Error):
-            i.run()
-        with pytest.raises(Error):
-            i.run('help')
-        with pytest.raises(Error):
-            i.run('xyzzybaz')
-
-        i.run('ping')
-        real.assert_called()
-        i.run('ping', destination='foo,bar')
-        assert real.call_args[1]['destination'], ['foo' == 'bar']
-        assert real.call_args[1]['timeout'] == 0.2
-        callback = real.call_args[1]['callback']
-
-        callback({'foo': {'ok': 'pong'}})
-        assert 'OK' in out.getvalue()
-
-        with patch('celery.bin.celery.dumps') as dumps:
-            i.run('ping', json=True)
-            dumps.assert_called()
-
-        instance = real.return_value = Mock()
-        instance._request.return_value = None
-        with pytest.raises(Error):
-            i.run('ping')
-
-        out.seek(0)
-        out.truncate()
-        i.quiet = True
-        i.say_chat('<-', 'hello')
-        assert not out.getvalue()
-
-
-class test_control:
-
-    def control(self, patch_call, *args, **kwargs):
-        kwargs.setdefault('app', Mock(name='app'))
-        c = control(*args, **kwargs)
-        if patch_call:
-            c.call = Mock(name='control.call')
-        return c
-
-    def test_call(self):
-        i = self.control(False)
-        i.call('foo', arguments={'kw': 2})
-        i.app.control.broadcast.assert_called_with(
-            'foo', arguments={'kw': 2}, reply=True)
 
 
 class test_multi:
---
+++ t/unit/bin/test_result.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import, unicode_literals
+from case import patch
+from celery.five import WhateverIO
+from celery.bin.result import result
+
+
+class test_result:
+
+    def setup(self):
+
+        @self.app.task(shared=False)
+        def add(x, y):
+            return x + y
+        self.add = add
+
+    def test_run(self):
+        with patch('celery.result.AsyncResult.get') as get:
+            out = WhateverIO()
+            r = result(app=self.app, stdout=out)
+            get.return_value = 'Jerry'
+            r.run('id')
+            assert 'Jerry' in out.getvalue()
+
+            get.return_value = 'Elaine'
+            r.run('id', task=self.add.name)
+            assert 'Elaine' in out.getvalue()
+
+            with patch('celery.result.AsyncResult.traceback') as tb:
+                r.run('id', task=self.add.name, traceback=True)
+                assert str(tb) in out.getvalue()
