abde096937fce3a3e849a0c95c59abd3a860fc61
Marat Sharafutdinov <decaz89@gmail.com>
2016-11-03 20:27:23 +0300

    Fix storing persistent data
    Using bytes literals for keys in shelve storage for working in Python 2.

--- celery/worker/state.py
+++ celery/worker/state.py
@@ -225,14 +225,14 @@ class Persistent(object):
 
     def _merge_clock(self, d):
         if self.clock:
-            d['clock'] = self.clock.adjust(d.get('clock') or 0)
+            d[b'clock'] = self.clock.adjust(d.get(b'clock') or 0)
 
     def _merge_revoked(self, d):
         try:
-            self._merge_revoked_v3(d['zrevoked'])
+            self._merge_revoked_v3(d[b'zrevoked'])
         except KeyError:
             try:
-                self._merge_revoked_v2(d.pop('revoked'))
+                self._merge_revoked_v2(d.pop(b'revoked'))
             except KeyError:
                 pass
         # purge expired items at boot
