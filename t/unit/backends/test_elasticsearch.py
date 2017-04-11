d3ec0ee5284dd9ee01d2a8781a7dd4c57d8ca054
Acey9 <huiwang.e@gmail.com>
2017-04-11 14:23:04 +0800

    fix unit test error

--- t/unit/backends/test_elasticsearch.py
+++ t/unit/backends/test_elasticsearch.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import pytest
 from case import Mock, sentinel, skip
 from celery.app import backends
+from celery.five import string
 from celery.backends import elasticsearch as module
 from celery.backends.elasticsearch import ElasticsearchBackend
 from celery.exceptions import ImproperlyConfigured
@@ -95,7 +96,7 @@ class test_ElasticsearchBackend:
         body = {"field1": "value1"}
         x._index(id=sentinel.task_id, body=body, kwarg1='test1')
         x._server.index.assert_called_once_with(
-            id=sentinel.task_id,
+            id=string(sentinel.task_id),
             doc_type=x.doc_type,
             index=x.index,
             body=body,
