921c810f5c8cc5f06299178a6699d02e25f14731
georgepsarakis <giwrgos.psarakis@gmail.com>
2017-04-16 13:48:07 +0300

    Improve SQS mock server startup script

--- .travis-ci/integration/sqs/start-sqs-mock-server.sh
+++ .travis-ci/integration/sqs/start-sqs-mock-server.sh
@@ -6,8 +6,7 @@ MOTO_DIRECTORY="$TRAVIS_BUILD_DIR/servers/moto"
 mkdir -p "$MOTO_DIRECTORY"
 cd $MOTO_DIRECTORY
 virtualenv moto-env
-git clone https://github.com/spulec/moto.git
-cd moto
-$MOTO_DIRECTORY/./moto-env/bin/pip install .[server]
-cd .. && rm -rf moto
+git clone https://github.com/spulec/moto.git moto-git
+$MOTO_DIRECTORY/./moto-env/bin/pip install -U pip
+cd moto-git && $MOTO_DIRECTORY/./moto-env/bin/pip install .[server]
 nohup $MOTO_DIRECTORY/./moto-env/bin/moto_server sqs -H 127.0.0.1 -p 80 &
