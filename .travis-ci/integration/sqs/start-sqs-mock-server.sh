ae3343201764d6e88bb83f5541ef37d002f4714b
georgepsarakis <giwrgos.psarakis@gmail.com>
2017-04-16 19:41:19 +0300

    Improve SQS mock server startup script

--- .travis-ci/integration/sqs/start-sqs-mock-server.sh
+++ .travis-ci/integration/sqs/start-sqs-mock-server.sh
@@ -2,6 +2,7 @@
 
 set -e -u
 
+sudo apt-get install -y supervisor
 MOTO_DIRECTORY="$TRAVIS_BUILD_DIR/servers/moto"
 mkdir -p "$MOTO_DIRECTORY"
 cd $MOTO_DIRECTORY
@@ -9,4 +10,12 @@ virtualenv moto-env
 git clone https://github.com/spulec/moto.git moto-git
 $MOTO_DIRECTORY/./moto-env/bin/pip install -U pip
 cd moto-git && $MOTO_DIRECTORY/./moto-env/bin/pip install .[server]
-nohup $MOTO_DIRECTORY/./moto-env/bin/moto_server sqs -H 127.0.0.1 -p 80 &
+
+SUPERVISORD_CONF_PATH=/etc/supervisor/conf.d/moto-server-sqs.conf
+echo '[program:moto-server-sqs]' | sudo tee "$SUPERVISORD_CONF_PATH"
+echo "command=$MOTO_DIRECTORY/./moto-env/bin/moto_server sqs -H 127.0.0.1 -p 80" | sudo tee -a "$SUPERVISORD_CONF_PATH"
+echo "user=root" | sudo tee -a "$SUPERVISORD_CONF_PATH"
+sudo service supervisor stop
+sudo service supervisor start
+sleep 10
+curl -v localhost
