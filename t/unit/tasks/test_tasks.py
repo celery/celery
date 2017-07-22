e14bd6d27dbe242383b9c16053d02d59e3adbc39
David Baumgold <david@davidbaumgold.com>
2017-07-21 22:48:31 -0400

    Put side_effect on patch lines

--- t/unit/tasks/test_tasks.py
+++ t/unit/tasks/test_tasks.py
@@ -279,9 +279,8 @@ class test_task_retries(TasksCase):
         self.autoretry_task.apply((1, 0))
         assert self.autoretry_task.iterations == 6
 
-    @patch('random.randrange')
+    @patch('random.randrange', side_effect=lambda i: i-1)
     def test_autoretry_backoff(self, randrange):
-        randrange.side_effect = lambda i: i - 1
         task = self.autoretry_backoff_task
         task.max_retries = 3
         task.iterations = 0
@@ -295,11 +294,9 @@ class test_task_retries(TasksCase):
         ]
         assert retry_call_countdowns == [1, 2, 4, 8]
 
-    @patch('random.randrange')
-    @patch('random.choice')
+    @patch('random.randrange', side_effect=lambda i: i-1)
+    @patch('random.choice', side_effect=lambda seq: seq[0])
     def test_autoretry_backoff_jitter(self, randchoice, randrange):
-        randrange.side_effect = lambda i: i - 1
-        randchoice.side_effect = lambda seq: seq[0]
         task = self.autoretry_backoff_jitter_task
         task.max_retries = 3
         task.iterations = 0
