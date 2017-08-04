e2d2c328175bec5330aaf4292c2426392b44fc17
Chris Caron <lead2gold@gmail.com>
2017-08-03 20:41:03 -0400

    handle update() calls where time is specified

--- celery/utils/collections.py
+++ celery/utils/collections.py
@@ -575,12 +575,14 @@ class LimitedSet(object):
     def add(self, item, now=None):
         # type: (Any, float) -> None
         """Add a new item, or reset the expiry time of an existing item."""
-        now = now or time.time()
         if item in self._data:
             self.discard(item)
-        if now <= self.last_added:
-            # Force uniqueness
-            now = self.last_added + 1e-6
+        if now is None:
+            # Get time
+            now = time.time()
+            if now <= self.last_added:
+                # Force uniqueness (because we're not a call from update())
+                now = self.last_added + 1e-6
         entry = (now, item)
         self._data[item] = entry
         heappush(self._heap, entry)
