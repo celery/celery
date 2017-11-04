ecb38f03c675a83a0c7c051b8414766b44b25a29
Omer Katz <omer.drow@gmail.com>
2017-11-04 19:09:20 +0200

    Ignore unused import since it monkeypatches the couchbase client.

--- celery/backends/couchbase.py
+++ celery/backends/couchbase.py
@@ -14,7 +14,7 @@ from .base import KeyValueStoreBackend
 try:
     import couchbase_ffi # noqa
 except ImportError:
-    pass
+    pass # noqa
 try:
     from couchbase import Couchbase
     from couchbase.connection import Connection
