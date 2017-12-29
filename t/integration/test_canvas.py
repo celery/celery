b2811985017c8570b0b9d092623bd519bfb1a636
georgepsarakis <giwrgos.psarakis@gmail.com>
2017-12-29 16:21:09 +0200

    Import sequence for isort check

--- t/integration/test_canvas.py
+++ t/integration/test_canvas.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
-import pytest
 from time import sleep
+
+import pytest
 from redis import StrictRedis
 
 from celery import chain, chord, group
