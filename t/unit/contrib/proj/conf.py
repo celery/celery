339dba04bbbbddce4a9b6a862ae79434d6a221c1
Leo Singer <leo.singer@ligo.org>
2018-03-10 00:21:20 -0500

    Put back undoc-members option in sphinx test (#4586)
    The test will fail without this due to the bug explained in #4584.

--- t/unit/contrib/proj/conf.py
+++ t/unit/contrib/proj/conf.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 extensions = ['celery.contrib.sphinx', 'sphinx.ext.autodoc']
-autodoc_default_flags = ['members']
+autodoc_default_flags = ['members', 'undoc-members']
 autosummary_generate = True
 
 sys.path.insert(0, os.path.abspath('.'))
