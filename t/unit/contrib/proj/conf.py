a6b3e2278ed38c153b07e38177794dd3d3e280af
Leo Singer <leo.singer@ligo.org>
2018-03-10 02:18:29 -0500

    Teach autodoc to document tasks if undoc-members is not set (#4588)
    Celery tasks created with the @task decorator have the property
    that `obj.__doc__` and `obj.__class__.__doc__` are equal, which
    trips up the logic in `sphinx.ext.autodoc` that is supposed to
    suppress repetition of class documentation in an instance of the
    class. We have to override that behavior so that autodoc picks
    up tasks whether or not autodoc's `undoc-members` flag is set.
    
    Because autodoc really works now, we also have to correctly
    check that the task was defined in the current module!
    
    Fixes #4587.

--- t/unit/contrib/proj/conf.py
+++ t/unit/contrib/proj/conf.py
@@ -3,8 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 import sys
 
-extensions = ['celery.contrib.sphinx', 'sphinx.ext.autodoc']
-autodoc_default_flags = ['members', 'undoc-members']
-autosummary_generate = True
+extensions = ['celery.contrib.sphinx']
+autodoc_default_flags = ['members']
 
 sys.path.insert(0, os.path.abspath('.'))
