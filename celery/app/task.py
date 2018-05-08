2636251a1249634258c1910c6400bf2ccf28b8ed
George Psarakis <giwrgos.psarakis@gmail.com>
2018-05-08 16:25:35 +0300

    Do not subscribe to Redis channels when results are ignored (#4709)
    * Add ignored property to AsyncResult
    
    * Set AsyncResult as ignored on task creation & bypass backend callback
    
    * Cancel subscription when calling AsyncResult.forget
    
    * Cancel pending result operations on AsyncResult destruction
    
    * Integration tests for Redis channel unsubscriptions (#4707)
    
    * Handle KeyError when resetting Redis client objects
    
    https://travis-ci.org/celery/celery/jobs/375637202

--- celery/app/task.py
+++ celery/app/task.py
@@ -525,6 +525,9 @@ class Task(object):
 
         preopts = self._get_exec_options()
         options = dict(preopts, **options) if options else preopts
+
+        options.setdefault('ignore_result', self.ignore_result)
+
         return app.send_task(
             self.name, args, kwargs, task_id=task_id, producer=producer,
             link=link, link_error=link_error, result_cls=self.AsyncResult,
