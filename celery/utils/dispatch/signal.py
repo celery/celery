6dca9194a09ed6b2db0760c9a81b398cffd12e22
Jonas Haag <jonas@lophus.org>
2018-05-09 13:20:30 +0200

    Factor WeakMethod logic and add comment

--- celery/utils/dispatch/signal.py
+++ celery/utils/dispatch/signal.py
@@ -37,6 +37,29 @@ def _make_id(target):  # pragma: no cover
     return id(target)
 
 
+def _boundmethod_safe_weakref(obj):
+    """Get weakref constructor appropriate for `obj`.  `obj` may be a bound method.
+
+    Bound method objects must be special-cased because they're usually garbage
+    collected immediately, even if the instance they're bound to persists.
+
+    Returns:
+        a (weakref constructor, main object) tuple. `weakref constructor` is
+        either :class:`weakref.ref` or :class:`weakref.WeakMethod`.  `main
+        object` is the instance that `obj` is bound to if it is a bound method;
+        otherwise `main object` is simply `obj.
+    """
+    try:
+        obj.__func__
+        obj.__self__
+        # Bound method
+        return WeakMethod, obj.__self__
+    except AttributeError:
+        # Not a bound method
+        return weakref.ref, obj
+
+
+
 NONE_ID = _make_id(None)
 
 NO_RECEIVERS = object()
@@ -179,16 +202,7 @@ class Signal(object):  # pragma: no cover
             lookup_key = (_make_id(receiver), _make_id(sender))
 
         if weak:
-            ref = weakref.ref
-            receiver_object = receiver
-            try:
-                receiver.__self__
-                receiver.__func__
-            except AttributeError:
-                pass
-            else:
-                ref = WeakMethod
-                receiver_object = receiver.__self__
+            ref, receiver_object = _boundmethod_safe_weakref(receiver)
             if PY3:
                 receiver = ref(receiver)
                 weakref.finalize(receiver_object, self._remove_receiver)
