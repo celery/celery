55f988e9c51fd32fa9629e85f703dbed95952ece
tothegump <tothegump@gmail.com>
2018-05-13 00:06:14 +0800

    bugfix: Chain with one task * Fixes #4498

--- celery/canvas.py
+++ celery/canvas.py
@@ -779,10 +779,9 @@ class chain(_chain):
     def __new__(cls, *tasks, **kwargs):
         # This forces `chain(X, Y, Z)` to work the same way as `X | Y | Z`
         if not kwargs and tasks:
-            if len(tasks) == 1 and is_list(tasks[0]):
-                # ensure chain(generator_expression) works.
-                tasks = tasks[0]
-            return reduce(operator.or_, tasks)
+            if len(tasks) != 1 or is_list(tasks[0]):
+                tasks = tasks[0] if len(tasks) == 1 else tasks
+                return reduce(operator.or_, tasks)
         return super(chain, cls).__new__(cls, *tasks, **kwargs)
 
 
