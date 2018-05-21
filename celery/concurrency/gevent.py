1a941f97ad9f19fe86c241072d21d1e3e6e19a73
Asif Saifuddin Auvi <auvipy@gmail.com>
2018-05-21 12:24:42 +0600

    [WIP] import from asynchronous instead of async and fix python 3.7 compat issues (#4679)
    * imported from renamed asynchronous module of kombu
    
    * imported from renamed asynchronous module of kombu
    
    * imported from renamed asynchronous module of kombu in utils timer2
    
    * update minimum kombu version

--- celery/concurrency/gevent.py
+++ celery/concurrency/gevent.py
@@ -2,7 +2,7 @@
 """Gevent execution pool."""
 from __future__ import absolute_import, unicode_literals
 
-from kombu.async import timer as _timer
+from kombu.asynchronous import timer as _timer
 from kombu.five import monotonic
 
 from . import base
