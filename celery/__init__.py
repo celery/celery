17b7f4e0187b03552d73d4acc3a3d78cff1359b2
Omer Katz <omer.drow@gmail.com>
2018-05-22 09:24:38 +0300

    Bump version: 4.2.0rc3 → 4.2.0rc4

--- celery/__init__.py
+++ celery/__init__.py
@@ -14,7 +14,7 @@ from collections import namedtuple
 
 SERIES = 'windowlicker'
 
-__version__ = '4.2.0rc3'
+__version__ = '4.2.0rc4'
 __author__ = 'Ask Solem'
 __contact__ = 'ask@celeryproject.org'
 __homepage__ = 'http://celeryproject.org'
