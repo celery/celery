bff690df23f9d8bdb76911f0ed53d30639cfb6f1
Dash Winterson <dashdanw@gmail.com>
2018-07-28 00:26:52 -0400

    Changed wording in docs for Task.retry() to mention the fact that it requeues (#4933)
    * changed wording for autodocs
    
    * Updated wording to be more descriptive

--- celery/app/task.py
+++ celery/app/task.py
@@ -592,7 +592,7 @@ class Task(object):
 
     def retry(self, args=None, kwargs=None, exc=None, throw=True,
               eta=None, countdown=None, max_retries=None, **options):
-        """Retry the task.
+        """Retry the task, adding it to the back of the queue.
 
         Example:
             >>> from imaginary_twitter_lib import Twitter
