21baef53c39bc1909fd6eee9a2a20e6ce851e88c
Joshua Engelman <j.aaron.engelman@gmail.com>
2018-10-13 09:54:30 -0500

    Deadlock on inqueue close (#4185) (#4997)
    * remove read socket from thread hub on queue close
    
    * removed socket for queue reader

--- celery/concurrency/asynpool.py
+++ celery/concurrency/asynpool.py
@@ -737,10 +737,10 @@ class AsynPool(_pool.Pool):
                     fileno_to_inq.pop(fd, None)
                     active_writes.discard(fd)
                     all_inqueues.discard(fd)
-                    hub_remove(fd)
             except KeyError:
                 pass
         self.on_inqueue_close = on_inqueue_close
+        self.hub_remove = hub_remove
 
         def schedule_writes(ready_fds, total_write_count=[0]):
             # Schedule write operation to ready file descriptor.
@@ -1246,6 +1246,7 @@ class AsynPool(_pool.Pool):
             if queue:
                 for sock in (queue._reader, queue._writer):
                     if not sock.closed:
+                        self.hub_remove(sock)
                         try:
                             sock.close()
                         except (IOError, OSError):
