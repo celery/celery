9fd7b17986f47a3124abf33f60ea3ad256a36df7
Chris Lee <sihrc.c.lee@gmail.com>
2018-11-05 13:34:20 -0500

    FIX: moving pubsub client creation out of the getter and into start method

--- celery/backends/redis.py
+++ celery/backends/redis.py
@@ -104,6 +104,12 @@ class ResultConsumer(BaseResultConsumer):
         self._maybe_cancel_ready_task(meta)
 
     def start(self, initial_task_id, **kwargs):
+        if self._pubsub is None:
+            self._thread.client = self.backend.create_client()
+            self._thread.pubsub = self._thread.client.pubsub(
+                ignore_subscribe_messages=True
+            )
+
         self._consume_from(initial_task_id)
 
     def on_wait_for_pending(self, result, **kwargs):
@@ -139,14 +145,7 @@ class ResultConsumer(BaseResultConsumer):
 
     @property
     def _pubsub(self):
-        if getattr(self._thread, "_pubsub", None) is None:
-            self._thread.client = self.backend.create_client()
-            self._thread.pubsub = self._thread.client.pubsub(
-                ignore_subscribe_messages=True
-            )
-
-        return self._thread.pubsub
-
+        return getattr(self._thread, "_pubsub", None)
 
 class RedisBackend(BaseKeyValueStoreBackend, AsyncBackendMixin):
     """Redis task result store."""
@@ -415,7 +414,7 @@ class RedisBackend(BaseKeyValueStoreBackend, AsyncBackendMixin):
 
     @cached_property
     def client(self):
-        return self._create_client(**self.connparams)
+        return self.create_client()
 
     def create_client(self):
         return self._create_client(**self.connparams)
