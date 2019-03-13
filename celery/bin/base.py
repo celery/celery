celery/celery

commit acd6025b7dc4db112a31020686fc8b15e1722c67

Added proper error messages in cases where app cannot be loaded. (#4990)
Previously we'd simply crash with an exception. Now a proper error message is displayed.

celery/bin/base.py
@@ -50,6 +50,16 @@ Unrecognized command-line arguments: {0}
 Try --help?
 """
 
+UNABLE_TO_LOAD_APP_MODULE_NOT_FOUND = """
+Unable to load celery application.
+The module {0} was not found.
+"""
+
+UNABLE_TO_LOAD_APP_APP_MISSING = """
+Unable to load celery application.
+{0}
+"""
+
 find_long_opt = re.compile(r'.+?(--.+?)(?:\s|,|$)')
 find_rst_ref = re.compile(r':\w+:`(.+?)`')
 find_rst_decl = re.compile(r'^\s*\.\. .+?::.+$')
@@ -270,7 +280,16 @@ class Command(object):
 
         # Dump version and exit if '--version' arg set.
         self.early_version(argv)
-        argv = self.setup_app_from_commandline(argv)
+        try:
+            argv = self.setup_app_from_commandline(argv)
+        except ModuleNotFoundError as e:
+            self.on_error(UNABLE_TO_LOAD_APP_MODULE_NOT_FOUND.format(e.name))
+            return EX_FAILURE
+        except AttributeError as e:
+            msg = e.args[0].capitalize()
+            self.on_error(UNABLE_TO_LOAD_APP_APP_MISSING.format(msg))
+            return EX_FAILURE
+
         self.prog_name = os.path.basename(argv[0])
         return self.handle_argv(self.prog_name, argv[1:])
 

commit b41bfd532bee31ac451be077aa3b27f565451b09

Fall back to ImportError on Python < 3.6.
ModuleNotFoundError was introduced in Python 3.6.
acd6025 broke support for anything below that version.

This commit fixes it.

Fixes #5383.

celery/bin/base.py
@@ -44,6 +44,10 @@ __all__ = (
 for warning in (CDeprecationWarning, CPendingDeprecationWarning):
     warnings.simplefilter('once', warning, 0)
 
+# TODO: Remove this once we drop support for Python < 3.6
+if sys.version_info < (3, 6):
+    ModuleNotFoundError = ImportError
+
 ARGV_DISABLED = """
 Unrecognized command-line arguments: {0}
 