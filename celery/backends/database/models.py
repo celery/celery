c14fc8f26bfffdf8ba65d0eca34961208589bf9a
fortnecc <christopher.fortner@sykes.com>
2019-04-30 23:52:57 -0500

    More pydoc fixes

--- celery/backends/database/models.py
+++ celery/backends/database/models.py
@@ -48,7 +48,7 @@ class Task(ResultModelBase):
 
 
 class TaskExtended(Task):
-    """ For the extend result."""
+    """For the extend result."""
 
     __tablename__ = 'celery_taskmeta'
     __table_args__ = {'sqlite_autoincrement': True, 'extend_existing': True}
