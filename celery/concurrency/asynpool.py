928912942653a04ef6eced6afd51bdbe5f440fa7
Matt Davis <matteius@gmail.com>
2019-05-15 11:24:51 -0400

    Potential fix and an example test case for the OSError: [Errno 9] Bad file descriptor. #4457 (#5499)
    * make astimezone call in localize more safe
    
    make astimezone call in localize more safe; with tests
    
    * Check pt. my progress on adding a test that does get into the code path but does yet get any closed FDs.
    
    * A working test that demos the very bug I am trying to fix!
    
    * Remove my debugging pprint statements
    
    * Remove my debugging pprint statements
    
    * Remove my debugging pprint statements
    
    * Work around for the closed fd issue
    
    * Add when my first PR for celery was merged to master.
    
    * Remove unused import in test.
    
    * See if they gets the veyor py2 build passing again.
    
    * Fix the test to pass on python2.7 as well as py3

--- celery/concurrency/asynpool.py
+++ celery/concurrency/asynpool.py
@@ -484,8 +484,16 @@ class AsynPool(_pool.Pool):
         [self._track_child_process(w, hub) for w in self._pool]
         # Handle_result_event is called whenever one of the
         # result queues are readable.
-        [hub.add_reader(fd, self.handle_result_event, fd)
-         for fd in self._fileno_to_outq]
+        stale_fds = []
+        for fd in self._fileno_to_outq:
+            try:
+                hub.add_reader(fd, self.handle_result_event, fd)
+            except OSError:
+                logger.info("Encountered OSError while trying "
+                            "to access fd %s ", fd, exc_info=True)
+                stale_fds.append(fd)  # take note of stale fd
+        for fd in stale_fds:  # Remove now defunct file descriptors
+            self._fileno_to_outq.pop(fd, None)
 
         # Timers include calling maintain_pool at a regular interval
         # to be certain processes are restarted.
@@ -1059,7 +1067,7 @@ class AsynPool(_pool.Pool):
         return inq, outq, synq
 
     def on_process_alive(self, pid):
-        """Called when reciving the :const:`WORKER_UP` message.
+        """Called when receiving the :const:`WORKER_UP` message.
 
         Marks the process as ready to receive work.
         """
--- t/unit/worker/test_worker.py
+++ t/unit/worker/test_worker.py
@@ -10,8 +10,9 @@ from threading import Event
 
 import pytest
 from amqp import ChannelError
-from case import Mock, patch, skip
+from case import Mock, mock, patch, skip
 from kombu import Connection
+from kombu.asynchronous import get_event_loop
 from kombu.common import QoS, ignore_errors
 from kombu.transport.base import Message
 from kombu.transport.memory import Transport
@@ -29,7 +30,7 @@ from celery.platforms import EX_FAILURE
 from celery.utils.nodenames import worker_direct
 from celery.utils.serialization import pickle
 from celery.utils.timer2 import Timer
-from celery.worker import components, consumer, state
+from celery.worker import autoscale, components, consumer, state
 from celery.worker import worker as worker_module
 from celery.worker.consumer import Consumer
 from celery.worker.pidbox import gPidbox
@@ -790,6 +791,55 @@ class test_WorkController(ConsumerCase):
             timer_cls='celery.utils.timer2.Timer',
         )
         assert worker.autoscaler
+
+    @pytest.mark.nothreads_not_lingering
+    @mock.sleepdeprived(module=autoscale)
+    def test_with_autoscaler_file_descriptor_safety(self):
+        # Given: a test celery worker instance with auto scaling
+        worker = self.create_worker(
+            autoscale=[10, 5], use_eventloop=True,
+            timer_cls='celery.utils.timer2.Timer',
+            threads=False,
+        )
+        # Given: This test requires a QoS defined on the worker consumer
+        worker.consumer.qos = qos = QoS(lambda prefetch_count: prefetch_count, 2)
+        qos.update()
+
+        # Given: We have started the worker pool
+        worker.pool.start()
+
+        # Then: the worker pool is the same as the autoscaler pool
+        auto_scaler = worker.autoscaler
+        assert worker.pool == auto_scaler.pool
+
+        # Given: Utilize kombu to get the global hub state
+        hub = get_event_loop()
+        # Given: Initial call the Async Pool to register events works fine
+        worker.pool.register_with_event_loop(hub)
+
+        # Create some mock queue message and read from them
+        _keep = [Mock(name='req{0}'.format(i)) for i in range(20)]
+        [state.task_reserved(m) for m in _keep]
+        auto_scaler.body()
+
+        # Simulate a file descriptor from the list is closed by the OS
+        # auto_scaler.force_scale_down(5)
+        # This actually works -- it releases the semaphore properly
+        # Same with calling .terminate() on the process directly
+        for fd, proc in worker.pool._pool._fileno_to_outq.items():
+            # however opening this fd as a file and closing it will do it
+            queue_worker_socket = open(str(fd), "w")
+            queue_worker_socket.close()
+            break  # Only need to do this once
+
+        # When: Calling again to register with event loop ...
+        worker.pool.register_with_event_loop(hub)
+
+        # Then: test did not raise "OSError: [Errno 9] Bad file descriptor!"
+
+        # Finally:  Clean up so the threads before/after fixture passes
+        worker.terminate()
+        worker.pool.terminate()
 
     def test_dont_stop_or_terminate(self):
         worker = self.app.WorkController(concurrency=1, loglevel=0)
