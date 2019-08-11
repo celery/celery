c06ae78bf6edd3ea7ed96aa2e3e541ef690ef6e7
Aliaksei Urbanski <aliaksei.u@getbraavo.com>
2019-08-11 21:23:46 +0300

    Fix termination of asynloop (#5671)
    Currently, asynloop hangs on termination in case when
    should_terminate is not None and should_stop is False.
    
    It seems reasonable to use state.maybe_shutdown here in order
    to fix the issue and, on the other hand, to simplify the code.
    
    Also, I believe that termination should have a higher priority
    than shutting down, so order of checking for termination and
    for shutting down should be changed.

--- celery/worker/state.py
+++ celery/worker/state.py
@@ -74,10 +74,10 @@ def reset_state():
 
 def maybe_shutdown():
     """Shutdown if flags have been set."""
-    if should_stop is not None and should_stop is not False:
-        raise WorkerShutdown(should_stop)
-    elif should_terminate is not None and should_terminate is not False:
+    if should_terminate is not None and should_terminate is not False:
         raise WorkerTerminate(should_terminate)
+    elif should_stop is not None and should_stop is not False:
+        raise WorkerShutdown(should_stop)
 
 
 def task_reserved(request,
