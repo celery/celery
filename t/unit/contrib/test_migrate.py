46028e94cfb5ffdcd5d2379214fe99458a7a17f8
Asif Saif Uddin (Auvi) <auvipy@gmail.com>
2019-08-13 13:13:00 +0600

    fixed flake8 error

--- t/unit/contrib/test_migrate.py
+++ t/unit/contrib/test_migrate.py
@@ -36,8 +36,7 @@ def Message(body, exchange='exchange', routing_key='rkey',
             'content_type': content_type,
             'content_encoding': content_encoding,
             'properties': {
-                'correlation_id': isinstance(body, dict)
-                and body['id'] or None
+                'correlation_id': isinstance(body, dict) and body['id'] or None
             }
         },
     )
