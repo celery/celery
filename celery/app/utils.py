51040628bed951c73b8f5982a2bb2b7efff0cac8
brogon <brogon@palandor.net>
2019-08-23 19:13:56 +0200

    Make result backend 'prefix-configurable' (#5291)
    * Look first for the configured 'result_backend'
    
    Use 'CELERY_RESULT_BACKEND' only as fallback.
    
    * Adapted to scribos version scheme
    
    * Removed scribos-related version changes.

--- celery/app/utils.py
+++ celery/app/utils.py
@@ -114,8 +114,7 @@ class Settings(ConfigurationView):
     def result_backend(self):
         return (
             os.environ.get('CELERY_RESULT_BACKEND') or
-            self.get('result_backend') or
-            self.get('CELERY_RESULT_BACKEND')
+            self.first('result_backend', 'CELERY_RESULT_BACKEND')
         )
 
     @property
