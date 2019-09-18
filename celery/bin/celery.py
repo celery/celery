631e8736d6a1b9655ad3df759802af6a6e4d0a81
Omer Katz <omer.drow@gmail.com>
2019-09-18 12:43:32 +0300

    Implement the `celery list bindings` command.

--- celery/bin/celery.py
+++ celery/bin/celery.py
@@ -506,6 +506,39 @@ def call(ctx, name, args, kwargs, eta, countdown, expires, serializer, queue, ex
     click.echo(task_id)
 
 
+@celery.group(name="list")
+def list_():
+    """Get info from broker.
+
+    Note:
+
+        For RabbitMQ the management plugin is required.
+    """
+
+
+@list_.command(cls=CeleryCommand)
+@click.pass_context
+def bindings(ctx):
+    """Inspect queue bindings."""
+
+    # TODO: Consider using a table formatter for this command.
+    app = ctx.obj['app']
+    with app.connection() as conn:
+        app.amqp.TaskConsumer(conn).declare()
+
+        try:
+            bindings = conn.manager.get_bindings()
+        except NotImplementedError:
+            raise click.UsageError('Your transport cannot list bindings.')
+
+        def fmt(q, e, r):
+            click.echo('{0:<28} {1:<28} {2}'.format(q, e, r))
+        fmt('Queue', 'Exchange', 'Routing Key')
+        fmt('-' * 16, '-' * 16, '-' * 16)
+        for b in bindings:
+            fmt(b['destination'], b['source'], b['routing_key'])
+
+
 def main() -> int:
     """Start celery umbrella command.
 
