91866139d6a1004423bf65be7b095f0455a42e16
Anton Kovalevich <kai3341@gmail.com>
2019-09-28 01:16:07 +0300

    add taskcls decorator implementation

--- celery/app/base.py
+++ celery/app/base.py
@@ -366,6 +366,58 @@ class Celery(object):
             'celery.bin.worker:worker', app=self
         ).execute_from_commandline(argv)
 
+    def taskcls(self, *args, name=None, **opts):
+        """Class decarator to create a task class from
+        any class with fabrac classmethod `task`.
+
+        Example:
+            .. code-block:: python
+
+                class BaseTask:
+                    "We can define common behavior here"
+
+                    def __init__(self, *args, **kwargs):
+                        "Any behavior"
+
+                    @classmethod
+                    def task(cls, *args, **kwargs):
+                        instance = cls(*args, **kwargs)
+                        return instance.main()
+
+                @app.taskcls
+                class SimpleTask(BaseTask):
+                    def main(self):
+                        ...
+
+                @app.taskcls(bind=True)
+                class AnotherTask(BaseTask):
+                    "You can pass here the same args as app.task"
+                    def main(self):
+                        ...
+
+        And sending task almost the same:
+            .. code-block:: python
+
+                SimpleTask.task.delay(...)
+
+        OR
+            .. code-block:: python
+
+                task = app.tasks['my_module.SimpleTask']
+                task.delay(...)
+        """
+
+        def inner_taskcls(cls):
+            task_name = name or self.gen_task_name(
+                cls.__name__,
+                cls.__module__,
+            )
+
+            cls.task = self.task(*args, name=task_name, **opts)(cls.task)
+            return cls
+
+        return inner_taskcls
+
     def task(self, *args, **opts):
         """Decorator to create a task class out of any callable.
 
