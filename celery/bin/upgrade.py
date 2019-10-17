263750356d93f418085f0796bbe4074030231d10
Omer Katz <omer.drow@gmail.com>
2019-10-17 14:16:16 +0300

    Implement the `celery upgrade settings` command.

---
+++ celery/bin/upgrade.py
@@ -0,0 +1,87 @@
+import codecs
+import sys
+
+import click
+
+from celery.app import defaults
+from celery.bin.base import CeleryCommand, CeleryOption
+from celery.utils.functional import pass1
+
+
+@click.group()
+def upgrade():
+    """Perform upgrade between versions."""
+
+
+def _slurp(filename):
+    # TODO: Handle case when file does not exist
+    with codecs.open(filename, 'r', 'utf-8') as read_fh:
+        return [line for line in read_fh]
+
+
+def _compat_key(self, key, namespace='CELERY'):
+    key = key.upper()
+    if not key.startswith(namespace):
+        key = '_'.join([namespace, key])
+    return key
+
+
+def _backup(filename, suffix='.orig'):
+    lines = []
+    backup_filename = ''.join([filename, suffix])
+    print('writing backup to {0}...'.format(backup_filename),
+          file=sys.stderr)
+    with codecs.open(filename, 'r', 'utf-8') as read_fh:
+        with codecs.open(backup_filename, 'w', 'utf-8') as backup_fh:
+            for line in read_fh:
+                backup_fh.write(line)
+                lines.append(line)
+    return lines
+
+
+def _to_new_key(line, keyfilter=pass1, source=defaults._TO_NEW_KEY):
+    # sort by length to avoid, for example, broker_transport overriding
+    # broker_transport_options.
+    for old_key in reversed(sorted(source, key=lambda x: len(x))):
+        new_line = line.replace(old_key, keyfilter(source[old_key]))
+        if line != new_line and 'CELERY_CELERY' not in new_line:
+            return 1, new_line  # only one match per line.
+    return 0, line
+
+
+@upgrade.command(cls=CeleryCommand)
+@click.argument('filename')
+@click.option('-django',
+              cls=CeleryOption,
+              is_flag=True,
+              help_group='Upgrading Options',
+              help='Upgrade Django project.')
+@click.option('-compat',
+              cls=CeleryOption,
+              is_flag=True,
+              help_group='Upgrading Options',
+              help='Maintain backwards compatibility.')
+@click.option('--no-backup',
+              cls=CeleryOption,
+              is_flag=True,
+              help_group='Upgrading Options',
+              help='Dont backup original files.')
+def settings(filename, django, compat, no_backup):
+    lines = _slurp(filename)
+    keyfilter = _compat_key if django or compat else pass1
+    print('processing {0}...'.format(filename), file=sys.stderr)
+    # gives list of tuples: ``(did_change, line_contents)``
+    new_lines = [
+        _to_new_key(line, keyfilter) for line in lines
+    ]
+    if any(n[0] for n in new_lines):  # did have changes
+        if not no_backup:
+            _backup(filename)
+        with codecs.open(filename, 'w', 'utf-8') as write_fh:
+            for _, line in new_lines:
+                write_fh.write(line)
+        print('Changes to your setting have been made!',
+              file=sys.stdout)
+    else:
+        print('Does not seem to require any changes :-)',
+              file=sys.stdout)
