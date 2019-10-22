fafbbfd5fb2bddb06d957d2c9ef481febcbf1f36
Omer Katz <omer.drow@gmail.com>
2019-10-22 21:40:28 +0300

    Implement the `celery control` command.

--- celery/bin/control.py
+++ celery/bin/control.py
@@ -3,7 +3,7 @@ from functools import partial
 import click
 from kombu.utils.json import dumps
 
-from celery.app.control import Inspect
+from celery.app.control import Inspect, Control
 from celery.bin.base import COMMA_SEPARATED_LIST, CeleryCommand, CeleryOption
 from celery.platforms import EX_UNAVAILABLE
 from celery.utils import text
@@ -61,7 +61,8 @@ def status(ctx, timeout, destination, json, **kwargs):
 
 
 @click.command(cls=CeleryCommand)
-@click.argument("action", type=click.Choice([choice for choice in dir(Inspect) if not choice.startswith('_')]))
+@click.argument("action", type=click.Choice(
+    [choice for choice in dir(Inspect) if not choice.startswith('_')]))
 @click.option('-t',
               '--timeout',
               cls=CeleryOption,
@@ -99,6 +100,52 @@ def inspect(ctx, action, timeout, destination, json, **kwargs):
     if json:
         ctx.obj.echo(dumps(replies))
     nodecount = len(replies)
-    if not kwargs.get('quiet', False):
+    if not ctx.obj.quiet:
         ctx.obj.echo('\n{0} {1} online.'.format(
             nodecount, text.pluralize(nodecount, 'node')))
+
+
+@click.command(cls=CeleryCommand)
+@click.argument("action", type=click.Choice([
+    choice for choice in dir(Control)
+    if (choice not in ('inspect', 'broadcast')
+        and choice.islower()
+        and not choice.startswith('_'))
+]))
+@click.option('-t',
+              '--timeout',
+              cls=CeleryOption,
+              type=float,
+              default=1.0,
+              help_group='Remote Control Options',
+              help='Timeout in seconds waiting for reply.')
+@click.option('-d',
+              '--destination',
+              cls=CeleryOption,
+              type=COMMA_SEPARATED_LIST,
+              help_group='Remote Control Options',
+              help='Comma separated list of destination node names.')
+@click.option('-j',
+              '--json',
+              cls=CeleryOption,
+              is_flag=True,
+              help_group='Remote Control Options',
+              help='Use json as output format.')
+@click.pass_context
+def control(ctx, action, timeout, destination, json, **kwargs):
+    """Workers remote control.
+
+    Availability: RabbitMQ (AMQP), Redis, and MongoDB transports."""
+    callback = None if json else partial(say_remote_command_reply, ctx,
+                                         show_reply=True)
+    replies = ctx.obj.app.control.broadcast(action, timeout=timeout,
+                                            destination=destination,
+                                            callback=callback,
+                                            reply=True)
+
+    if not replies:
+        ctx.obj.echo('No nodes replied within time constraint')
+        return EX_UNAVAILABLE
+
+    if json:
+        ctx.obj.echo(dumps(replies))
