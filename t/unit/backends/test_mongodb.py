08ad7f25fbdfe9eafab72370b0ab45dd8fee2b4c
spengjie <spengjie@sina.com>
2019-10-28 14:02:08 +0800

    Fix #5734 Celery does not consider authMechanism on mongodb backend URLs (#5795)
    * Fix #5734 Celery does not consider authMechanism on mongodb backend URLs
    
    * Add unit test: test_get_connection_with_authmechanism
    
    * Add unit test: test_get_connection_with_authmechanism_no_username
    
    * Fix errors in Python 2.7
    
    Remove "," after "**" operator

--- t/unit/backends/test_mongodb.py
+++ t/unit/backends/test_mongodb.py
@@ -5,6 +5,7 @@ from pickle import dumps, loads
 
 import pytest
 from kombu.exceptions import EncodeError
+from pymongo.errors import ConfigurationError
 
 from case import ANY, MagicMock, Mock, mock, patch, sentinel, skip
 from celery import states, uuid
@@ -219,6 +220,42 @@ class test_MongoBackend:
                 host=mongodb_uri, **self.backend._prepare_client_options()
             )
             assert sentinel.connection == connection
+
+    def test_get_connection_with_authmechanism(self):
+        with patch('pymongo.MongoClient') as mock_Connection:
+            self.app.conf.mongodb_backend_settings = None
+            uri = ('mongodb://'
+                   'celeryuser:celerypassword@'
+                   'localhost:27017/'
+                   'celerydatabase?authMechanism=SCRAM-SHA-256')
+            mb = MongoBackend(app=self.app, url=uri)
+            mock_Connection.return_value = sentinel.connection
+            connection = mb._get_connection()
+            mock_Connection.assert_called_once_with(
+                host=['localhost:27017'],
+                username='celeryuser',
+                password='celerypassword',
+                authmechanism='SCRAM-SHA-256',
+                **mb._prepare_client_options()
+            )
+            assert sentinel.connection == connection
+
+    def test_get_connection_with_authmechanism_no_username(self):
+        with patch('pymongo.MongoClient') as mock_Connection:
+            self.app.conf.mongodb_backend_settings = None
+            uri = ('mongodb://'
+                   'localhost:27017/'
+                   'celerydatabase?authMechanism=SCRAM-SHA-256')
+            mb = MongoBackend(app=self.app, url=uri)
+            mock_Connection.side_effect = ConfigurationError(
+                'SCRAM-SHA-256 requires a username.')
+            with pytest.raises(ConfigurationError):
+                mb._get_connection()
+            mock_Connection.assert_called_once_with(
+                host=['localhost:27017'],
+                authmechanism='SCRAM-SHA-256',
+                **mb._prepare_client_options()
+            )
 
     @patch('celery.backends.mongodb.MongoBackend._get_connection')
     def test_get_database_no_existing(self, mock_get_connection):
