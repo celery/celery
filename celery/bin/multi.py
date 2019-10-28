a02d87ee252ce2a30e0b65cab94a5f85affb78cf
Omer Katz <omer.drow@gmail.com>
2019-10-28 16:14:19 +0200

    Isort.

--- celery/bin/multi.py
+++ celery/bin/multi.py
@@ -97,18 +97,15 @@ import signal
 import sys
 from functools import wraps
 
+import click
 from kombu.utils.objects import cached_property
 
 from celery import VERSION_BANNER
 from celery.apps.multi import Cluster, MultiParser, NamespacedOptionParser
+from celery.bin.base import CeleryCommand
 from celery.platforms import EX_FAILURE, EX_OK, signals
 from celery.utils import term
 from celery.utils.text import pluralize
-
-import click
-
-from celery.bin.base import CeleryCommand
-
 
 __all__ = ('MultiTool',)
 
