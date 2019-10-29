534294cdd9e30aae5636f79509331ca4ba80c650
Asif Saif Uddin <auvipy@gmail.com>
2019-10-29 09:23:13 +0600

    py 3.8 in clasifier

--- setup.py
+++ setup.py
@@ -100,6 +100,7 @@ classes = """
     Programming Language :: Python :: 3.5
     Programming Language :: Python :: 3.6
     Programming Language :: Python :: 3.7
+    Programming Language :: Python :: 3.8
     Programming Language :: Python :: Implementation :: CPython
     Programming Language :: Python :: Implementation :: PyPy
     Operating System :: OS Independent
