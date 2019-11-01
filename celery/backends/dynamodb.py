b5bf08911e6f8592c7a63ba0be9842f607e88352
Sven Ulland <sven.ulland+github@xeneta.com>
2019-11-01 21:29:01 +0100

    Improve logging
    - Handle exceptions by logging the error and re-raising
    
    - Log (level debug) when the desired TTL state is already in place

--- celery/backends/dynamodb.py
+++ celery/backends/dynamodb.py
@@ -268,12 +268,30 @@ class DynamoDBBackend(KeyValueStoreBackend):
                 if cur_attr_name == self._ttl_field.name:
                     # We want TTL enabled, and it is currently enabled or being
                     # enabled, and on the correct attribute.
+                    logger.debug((
+                        'DynamoDB Time to Live is {situation} '
+                        'on table {table}'
+                    ).format(
+                        situation='already enabled' \
+                            if status == 'ENABLED' \
+                            else 'currently being enabled',
+                        table=self.table_name
+                    ))
                     return description
 
         elif status in ('DISABLED', 'DISABLING'):
             if self.time_to_live_seconds is None:
                 # We want TTL disabled, and it is currently disabled or being
                 # disabled.
+                logger.debug((
+                    'DynamoDB Time to Live is {situation} '
+                    'on table {table}'
+                ).format(
+                    situation='already disabled' \
+                        if status == 'DISABLED' \
+                        else 'currently being disabled',
+                    table=self.table_name
+                ))
                 return description
 
         # At this point, we have one of the following situations:
@@ -307,19 +325,35 @@ class DynamoDBBackend(KeyValueStoreBackend):
         #
         attr_name = \
             cur_attr_name if status == 'ENABLED' else self._ttl_field.name
-        specification = self._client.update_time_to_live(
-            **self._get_ttl_specification(
-                ttl_attr_name=attr_name
+        try:
+            specification = self._client.update_time_to_live(
+                **self._get_ttl_specification(
+                    ttl_attr_name=attr_name
+                )
             )
-        )
-        logger.info(
-            'DynamoDB Table {} time-to-live enabled={} on field {}.'.format(
-                self.table_name,
-                False if self.time_to_live_seconds is None else True,
-                self._ttl_field.name
+            logger.info(
+                'DynamoDB Table {} time-to-live enabled={} on field {}.'.format(
+                    self.table_name,
+                    False if self.time_to_live_seconds is None else True,
+                    self._ttl_field.name
+                )
             )
-        )
-        return specification
+            return specification
+        except ClientError as e:
+            error_code = e.response['Error'].get('Code', 'Unknown')
+            error_message = e.response['Error'].get('Message', 'Unknown')
+            logger.error((
+                'Error {action} Time to Live on DynamoDB table {table}: '
+                '{code}: {message}'
+            ).format(
+                action='disabling' \
+                    if self.time_to_live_seconds is None \
+                    else 'enabling',
+                table=self.table_name,
+                code=error_code,
+                message=error_message,
+            ))
+            raise e
 
     def _wait_for_table_status(self, expected='ACTIVE'):
         """Poll for the expected table status."""
