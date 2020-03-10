c711047036819cbe003dcfbd81fc35bb0b4af5d3
Clement Michaud <c.michaud@criteo.com>
2020-03-10 16:44:05 +0100

    Avoid race condition due to task duplication.
    In some circumstances like a network partitioning, some tasks might
    be duplicated. Sometimes, this lead to a race condition where a lost
    task overwrites the result of the last successful task in the backend.
    
    In order to avoid this race condition we prevent updating the result if
    it's already in successful state.
    
    This fix has been done for KV backends only and therefore won't work
    with other backends.

--- celery/backends/base.py
+++ celery/backends/base.py
@@ -768,6 +768,17 @@ class BaseKeyValueStoreBackend(Backend):
                                      traceback=traceback, request=request)
         meta['task_id'] = bytes_to_str(task_id)
 
+        # Retrieve metadata from the backend, if the status
+        # is a success then we ignore any following update to the state.
+        # This solves a task deduplication issue because of network
+        # partitioning or lost workers. This issue involved a race condition
+        # making a lost task overwrite the last successful result in the
+        # result backend.
+        current_meta = self._get_task_meta_for(task_id)
+
+        if current_meta['status'] == states.SUCCESS:
+            return result
+
         self.set(self.get_key_for_task(task_id), self.encode(meta))
         return result
 
