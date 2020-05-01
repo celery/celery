657206abe0a3d3525ade165a85c846ca2bd618a6
Asif Saif Uddin <auvipy@gmail.com>
2020-05-01 18:01:00 +0600

    update setup.py

--- setup.py
+++ setup.py
@@ -204,7 +204,7 @@ setuptools.setup(
     license='BSD',
     platforms=['any'],
     install_requires=install_requires(),
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*,",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     tests_require=reqs('test.txt'),
     extras_require=extras_require(),
     cmdclass={'test': pytest},
