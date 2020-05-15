f99ba459de36c91b1442a3ead3d6199da951ba79
Asif Saif Uddin <auvipy@gmail.com>
2020-05-15 08:24:21 +0600

    pypy3

--- setup.py
+++ setup.py
@@ -239,6 +239,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: Implementation :: PyPy3",
         "Operating System :: OS Independent"
     ]
 )
