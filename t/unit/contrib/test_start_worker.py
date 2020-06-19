44e6b8509649c578216b5184046e2614f56ee3b2
Gordon Pendleton <wgordonw1@gmail.com>
2020-06-19 10:40:06 -0400

    added test for unspecified hostname case

--- t/unit/contrib/test_start_worker.py
+++ t/unit/contrib/test_start_worker.py
@@ -1,7 +1,12 @@
 from celery.contrib.testing.worker import start_worker
+from celery.utils.nodenames import anon_nodename
 
 
 class test_StartWorker:
     def test_start_worker_hostname(self):
         with start_worker(self.app, hostname='foo') as worker:
             assert worker.hostname == 'celery@foo'
+    
+    def test_start_worker_hostname_unspecified(self):
+        with start_worker(self.app) as worker:
+            assert worker.hostname == anon_nodename()
