0c712bc783faf1fefaadf07be673c49ba967eb7b
Mathieu Chataigner <m.chataigner@criteo.com>
2020-07-01 18:19:22 +0200

    Trying to fix flaky tests in ci

--- t/unit/concurrency/test_thread.py
+++ t/unit/concurrency/test_thread.py
@@ -12,12 +12,18 @@ class test_thread_TaskPool:
     def test_on_apply(self):
         from celery.concurrency import thread
         x = thread.TaskPool()
-        x.on_apply(operator.add, (2, 2), {}, noop, noop)
+        try:
+            x.on_apply(operator.add, (2, 2), {}, noop, noop)
+        finally:
+            x.stop()
 
     def test_info(self):
         from celery.concurrency import thread
         x = thread.TaskPool()
-        assert x.info
+        try:
+            assert x.info
+        finally:
+            x.stop()
 
     def test_on_stop(self):
         from celery.concurrency import thread
