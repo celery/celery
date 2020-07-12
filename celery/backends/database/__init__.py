0d23435f556cca4a23acf9d12fe4fe79944ac2bb
Omer Katz <omer.drow@gmail.com>
2020-07-12 11:56:48 +0300

    Remove five from celery/backends/database/__init__.py

--- celery/backends/database/__init__.py
+++ celery/backends/database/__init__.py
@@ -7,9 +7,7 @@ from vine.utils import wraps
 from celery import states
 from celery.backends.base import BaseBackend
 from celery.exceptions import ImproperlyConfigured
-from celery.five import range
 from celery.utils.time import maybe_timedelta
-
 from .models import Task, TaskExtended, TaskSet
 from .session import SessionManager
 
