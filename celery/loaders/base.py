2734754a9865bd96154aa5992ad92fa6e25262b0
Omer Katz <omer.drow@gmail.com>
2020-07-12 12:05:20 +0300

    Remove five from celery/loaders/base.py

--- celery/loaders/base.py
+++ celery/loaders/base.py
@@ -2,14 +2,12 @@
 import importlib
 import os
 import re
-import sys
 from datetime import datetime
 
 from kombu.utils import json
 from kombu.utils.objects import cached_property
 
 from celery import signals
-from celery.five import reraise, string_t
 from celery.utils.collections import DictAttribute, force_mapping
 from celery.utils.functional import maybe_list
 from celery.utils.imports import (NotAPackage, find_module, import_from_cwd,
@@ -118,7 +116,7 @@ class BaseLoader:
         self.on_worker_process_init()
 
     def config_from_object(self, obj, silent=False):
-        if isinstance(obj, string_t):
+        if isinstance(obj, str):
             try:
                 obj = self._smart_import(obj, imp=self.import_from_cwd)
             except (ImportError, AttributeError):
@@ -146,12 +144,10 @@ class BaseLoader:
     def _import_config_module(self, name):
         try:
             self.find_module(name)
-        except NotAPackage:
+        except NotAPackage as exc:
             if name.endswith('.py'):
-                reraise(NotAPackage, NotAPackage(CONFIG_WITH_SUFFIX.format(
-                    module=name, suggest=name[:-3])), sys.exc_info()[2])
-            reraise(NotAPackage, NotAPackage(CONFIG_INVALID_NAME.format(
-                module=name)), sys.exc_info()[2])
+                raise NotAPackage(CONFIG_WITH_SUFFIX.format(module=name, suggest=name[:-3])) from exc
+            raise NotAPackage(CONFIG_INVALID_NAME.format(module=name)) from exc
         else:
             return self.import_from_cwd(name)
 
