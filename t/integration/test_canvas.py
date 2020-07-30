1f4af2d6c19ba83ec751fa2d71adc3ea232d0c21
Thomas Grainger <tagrain@gmail.com>
2020-07-30 11:21:27 +0100

    fix flaky test_add_chord_to_chord
    Fixes #6256
    related https://github.com/celery/celery/pull/6218/files#diff-2ae8afebeb9ba0fd1534a70264a2ac68R621

--- t/integration/test_canvas.py
+++ t/integration/test_canvas.py
@@ -630,7 +630,7 @@ class test_chord:
 
         c = group([add_chord_to_chord.s([1, 2, 3], 4)]) | identity.s()
         res = c()
-        assert res.get() == [0, 5 + 6 + 7]
+        assert sorted(res.get()) == [0, 5 + 6 + 7]
 
     @flaky
     def test_eager_chord_inside_task(self, manager):
