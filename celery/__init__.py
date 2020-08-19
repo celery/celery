302bc8e6349cdfd723abb558dd5a330052ccffdd
Omer Katz <omer.drow@gmail.com>
2020-08-19 20:25:54 +0300

    Bump version: 5.0.0a2 → 5.0.0b1

--- celery/__init__.py
+++ celery/__init__.py
@@ -17,7 +17,7 @@ from . import local  # noqa
 
 SERIES = 'cliffs'
 
-__version__ = '5.0.0a2'
+__version__ = '5.0.0b1'
 __author__ = 'Ask Solem'
 __contact__ = 'auvipy@gmail.com'
 __homepage__ = 'http://celeryproject.org'
