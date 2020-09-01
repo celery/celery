54356fb8c222c6f7375a2579e1c75a0bab13c111
Omer Katz <omer.drow@gmail.com>
2020-09-01 14:52:00 +0300

    Remove compatibility code for gevent<1.0.

--- celery/bin/worker.py
+++ celery/bin/worker.py
@@ -33,12 +33,6 @@ def maybe_patch_concurrency(library):
         import gevent.signal
 
         gevent.monkey.patch_all()
-        if gevent.version_info[0] == 0:  # pragma: no cover
-            # Signals aren't working in gevent versions <1.0,
-            # and aren't monkey patched by patch_all()
-            import signal
-
-            signal.signal = gevent.signal
 
     patches = {
         'eventlet': patch_eventlet,
