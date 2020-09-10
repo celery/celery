23a22b00ce87cd4852f627d769139520585c5b5d
Omer Katz <omer.drow@gmail.com>
2020-09-10 12:51:16 +0300

    Bump version: 4.3.0 → 4.3.1

--- celery/__init__.py
+++ celery/__init__.py
@@ -14,7 +14,7 @@ from collections import namedtuple
 
 SERIES = 'rhubarb'
 
-__version__ = '4.3.0'
+__version__ = '4.3.1'
 __author__ = 'Ask Solem'
 __contact__ = 'ask@celeryproject.org'
 __homepage__ = 'http://celeryproject.org'
