a1fdc0211d12d158890aef249801912d7f0c7d2e
Sven Koitka <sven.koitka@uk-essen.de>
2020-12-10 08:16:52 +0100

    Convert key from bytes to str

--- celery/backends/s3.py
+++ celery/backends/s3.py
@@ -72,6 +72,7 @@ class S3Backend(KeyValueStoreBackend):
         s3_object.put(Body=value)
 
     def delete(self, key):
+        key = bytes_to_str(key)
         s3_object = self._get_s3_object(key)
         s3_object.delete()
 
