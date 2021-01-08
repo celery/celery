23e054778bf5f9b3b963feda1f797c7969603685
Jorrit <jorrit@wehelpen.nl>
2021-01-08 14:02:17 +0100

    pass_context for handle_preload_options decorator
    handle_reload_options requires the ctx argument.

--- celery/bin/list.py
+++ celery/bin/list.py
@@ -5,8 +5,9 @@ from celery.bin.base import CeleryCommand, handle_preload_options
 
 
 @click.group(name="list")
+@click.pass_context
 @handle_preload_options
-def list_():
+def list_(ctx):
     """Get info from broker.
 
     Note:
