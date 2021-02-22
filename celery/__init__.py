f33a473de740a00ea70a53fbfb8d91007a793281
Ionel Cristian Mărieș <contact@ionelmc.ro>
2021-02-22 15:08:23 +0200

    PEP440 conformat local version.

--- celery/__init__.py
+++ celery/__init__.py
@@ -14,7 +14,7 @@ from collections import namedtuple
 
 SERIES = 'latentcall'
 
-__version__ = '4.1.1-sl.2'
+__version__ = '4.1.1+sl.2'
 __author__ = 'Ask Solem'
 __contact__ = 'ask@celeryproject.org'
 __homepage__ = 'http://celeryproject.org'
