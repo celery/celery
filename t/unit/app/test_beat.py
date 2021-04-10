celery/celery

commit 1c955bf76a9a33052d87f2bccf0889fb21f27d41

Safeguard against schedule entry without kwargs (#6619)
* Possible fix for #5889

verification & test needed

* Update celery/beat.py

Co-authored-by: Omer Katz <omer.drow@gmail.com>

* Extract logic to methods.

* Add test.

Co-authored-by: Omer Katz <omer.drow@gmail.com>

celery/beat.py
@@ -203,6 +203,24 @@ class ScheduleEntry:
         return not self == other
 
 
+def _evaluate_entry_args(entry_args):
+    if not entry_args:
+        return []
+    return [
+        v() if isinstance(v, BeatLazyFunc) else v
+        for v in entry_args.args
+    ]
+
+
+def _evaluate_entry_kwargs(entry_kwargs):
+    if not entry_kwargs:
+        return {}
+    return {
+        k: v() if isinstance(v, BeatLazyFunc) else v
+        for k, v in entry_kwargs.items()
+    }
+
+
 class Scheduler:
     """Scheduler for periodic tasks.
 
@@ -380,8 +398,8 @@ class Scheduler:
         task = self.app.tasks.get(entry.task)
 
         try:
-            entry_args = [v() if isinstance(v, BeatLazyFunc) else v for v in (entry.args or [])]
-            entry_kwargs = {k: v() if isinstance(v, BeatLazyFunc) else v for k, v in entry.kwargs.items()}
+            entry_args = _evaluate_entry_args(entry.args)
+            entry_kwargs = _evaluate_entry_kwargs(entry.kwargs)
             if task:
                 return task.apply_async(entry_args, entry_kwargs,
                                         producer=producer,

t/unit/app/test_beat.py
@@ -196,6 +196,22 @@ class test_Scheduler:
         scheduler.apply_async(scheduler.Entry(task=foo.name, app=self.app, args=None, kwargs=None))
         foo.apply_async.assert_called()
 
+    def test_apply_async_with_null_args_set_to_none(self):
+
+        @self.app.task(shared=False)
+        def foo():
+            pass
+        foo.apply_async = Mock(name='foo.apply_async')
+
+        scheduler = mScheduler(app=self.app)
+        entry = scheduler.Entry(task=foo.name, app=self.app, args=None,
+                                kwargs=None)
+        entry.args = None
+        entry.kwargs = None
+
+        scheduler.apply_async(entry, advance=False)
+        foo.apply_async.assert_called()
+
     def test_should_sync(self):
 
         @self.app.task(shared=False)

commit 07b372022662e3e77ba96267d6d354bc71560f47

fix AttributeError regression in #6619

celery/beat.py
@@ -208,7 +208,7 @@ def _evaluate_entry_args(entry_args):
         return []
     return [
         v() if isinstance(v, BeatLazyFunc) else v
-        for v in entry_args.args
+        for v in entry_args
     ]
 
 

t/unit/app/test_beat.py
@@ -212,6 +212,23 @@ class test_Scheduler:
         scheduler.apply_async(entry, advance=False)
         foo.apply_async.assert_called()
 
+    def test_apply_async_without_null_args(self):
+
+        @self.app.task(shared=False)
+        def foo(moo: int):
+            return moo
+        foo.apply_async = Mock(name='foo.apply_async')
+
+        scheduler = mScheduler(app=self.app)
+        entry = scheduler.Entry(task=foo.name, app=self.app, args=None,
+                                kwargs=None)
+        entry.args = (101,)
+        entry.kwargs = None
+
+        scheduler.apply_async(entry, advance=False)
+        foo.apply_async.assert_called()
+        assert foo.apply_async.call_args[0][0] == [101]
+
     def test_should_sync(self):
 
         @self.app.task(shared=False)