ca489c6f7767ed796bce10400321fe08b4820c0c
Omer Katz <omer.drow@gmail.com>
2021-07-14 03:18:18 +0300

    Make ``ResultSet.on_ready`` promise hold a weakref to self. (#6784)

--- celery/result.py
+++ celery/result.py
@@ -2,6 +2,7 @@
 
 import datetime
 import time
+from weakref import proxy
 from collections import deque
 from contextlib import contextmanager
 
@@ -535,7 +536,7 @@ class ResultSet(ResultBase):
     def __init__(self, results, app=None, ready_barrier=None, **kwargs):
         self._app = app
         self.results = results
-        self.on_ready = promise(args=(self,))
+        self.on_ready = promise(args=(proxy(self),))
         self._on_full = ready_barrier or barrier(results)
         if self._on_full:
             self._on_full.then(promise(self._on_ready, weak=True))
