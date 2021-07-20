bffa201acc966c59ca40c1e3e7f39d21630f3c96
Thomas Grainger <tagrain@gmail.com>
2021-07-20 09:16:03 +0100

    apply pre-commit run --all-files

--- celery/bin/worker.py
+++ celery/bin/worker.py
@@ -8,10 +8,10 @@ from click import ParamType
 from click.types import StringParamType
 
 from celery import concurrency
-from celery.concurrency.base import BasePool
 from celery.bin.base import (COMMA_SEPARATED_LIST, LOG_LEVEL,
                              CeleryDaemonCommand, CeleryOption,
                              handle_preload_options)
+from celery.concurrency.base import BasePool
 from celery.exceptions import SecurityError
 from celery.platforms import (EX_FAILURE, EX_OK, detached,
                               maybe_drop_privileges)
