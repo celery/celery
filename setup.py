b2056b62c12615df88019a0f1c5b3935480a4037
Thomas Grainger <tagrain@gmail.com>
2021-08-14 08:54:26 +0100

    add trove classifier for py3.10

--- setup.py
+++ setup.py
@@ -192,6 +192,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Operating System :: OS Independent"
