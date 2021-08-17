2ac331026fab3d40ba1b2d106058356c30b48cb6
Omer Katz <omer.drow@gmail.com>
2021-08-17 17:34:33 +0300

    Bump version: 5.2.0b1 → 5.2.0b2

--- celery/__init__.py
+++ celery/__init__.py
@@ -17,7 +17,7 @@ from . import local
 
 SERIES = 'dawn-chorus'
 
-__version__ = '5.2.0b1'
+__version__ = '5.2.0b2'
 __author__ = 'Ask Solem'
 __contact__ = 'auvipy@gmail.com'
 __homepage__ = 'http://celeryproject.org'
