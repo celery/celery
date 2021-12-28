04e924edc8e9ee70b5ee963a645f717a1a3dd035
Asif Saif Uddin <auvipy@gmail.com>
2021-12-28 09:15:32 +0600

    try to make linters happy

--- celery/backends/base.py
+++ celery/backends/base.py
@@ -16,7 +16,7 @@ from weakref import WeakValueDictionary
 from billiard.einfo import ExceptionInfo
 from kombu.serialization import dumps, loads, prepare_accept_content
 from kombu.serialization import registry as serializer_registry
-from kombu.utils.encoding import bytes_to_str, ensure_bytes, from_utf8
+from kombu.utils.encoding import bytes_to_str, ensure_bytes
 from kombu.utils.url import maybe_sanitize_url
 
 import celery.exceptions
