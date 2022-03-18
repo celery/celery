887406250250b9035446126d8096d6d8862ffba7
Oleg Hoefling <oleg.hoefling@gmail.com>
2022-03-18 13:04:29 +0100

    doc: fix broken reference to schedule.is_due method
    Signed-off-by: Oleg Hoefling <oleg.hoefling@gmail.com>

--- celery/beat.py
+++ celery/beat.py
@@ -156,7 +156,7 @@ class ScheduleEntry:
         })
 
     def is_due(self):
-        """See :meth:`~celery.schedule.schedule.is_due`."""
+        """See :meth:`~celery.schedules.schedule.is_due`."""
         return self.schedule.is_due(self.last_run_at)
 
     def __iter__(self):
