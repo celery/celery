7ff7071e3930dfdbd40fffc3d8ffbef0722bdc48
Omer Katz <omer.katz@kcg.tech>
2022-04-05 11:34:48 +0300

    Revert "Bump version: 5.2.5 → 5.2.6"
    This reverts commit 272b534bb7e015617b3b9da63fbdaef3c8beff98.

--- celery/__init__.py
+++ celery/__init__.py
@@ -17,7 +17,7 @@ from . import local
 
 SERIES = 'dawn-chorus'
 
-__version__ = '5.2.6'
+__version__ = '5.2.5'
 __author__ = 'Ask Solem'
 __contact__ = 'auvipy@gmail.com'
 __homepage__ = 'http://celeryproject.org'
