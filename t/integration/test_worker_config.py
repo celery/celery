ee258e6cfdfec82b301be0d88b8267e71394911c
Tom Parker-Shemilt <tom.parker-shemilt@babylonhealth.com>
2022-06-01 16:12:04 +0100

    Add fast-fail coverage to worker

---
+++ t/integration/test_worker_config.py
@@ -0,0 +1,8 @@
+# Test config for t/integration/test_worker.py
+
+# Fail fast for test_run_worker
+broker_connection_retry_on_startup = False
+broker_connection_retry = False
+broker_connection_timeout = 0
+
+worker_log_color = False
