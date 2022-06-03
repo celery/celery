84851aa5cfe95017e9120929692d4303ef89fcf7
Tom Parker-Shemilt <palfrey@tevp.net>
2022-06-03 15:01:43 +0100

    Use test celery project

--- t/unit/bin/test_worker.py
+++ t/unit/bin/test_worker.py
@@ -14,7 +14,7 @@ def test_cli(isolated_cli_runner: CliRunner):
     Logging._setup = True  # To avoid hitting the logging sanity checks
     res = isolated_cli_runner.invoke(
         celery,
-        ["--config", "t.integration.test_worker_config", "worker"],
+        ["-A", "t.unit.bin.proj.app", "worker"],
         catch_exceptions=False
     )
-    assert res.exit_code == 1, res
+    assert res.exit_code == 1, (res, res.stdout)
