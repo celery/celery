c76ef0c6a5a51cef1aea73f84784dfb162781a1c
woutdenolf <woutdenolf@users.sf.net>
2022-10-03 11:44:00 +0200

    importlib_metadata removed deprecated entry point interfaces

--- celery/utils/imports.py
+++ celery/utils/imports.py
@@ -141,7 +141,14 @@ def gen_task_name(app, name, module_name):
 
 
 def load_extension_class_names(namespace):
-    for ep in entry_points().get(namespace, []):
+    if sys.version_info >= (3, 10):
+        _entry_points = entry_points(group=namespace)
+    else:
+        try:
+            _entry_points = entry_points().get(namespace, [])
+        except AttributeError:
+            _entry_points = entry_points().select(group=namespace)
+    for ep in _entry_points:
         yield ep.name, ep.value
 
 
