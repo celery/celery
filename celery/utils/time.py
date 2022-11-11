e3e96f7b700734fe6cbe9d86f9e6e3bcc5f8fd03
Andrew Yoo <andrew.yoo@whova.com>
2022-11-11 10:25:56 -0800

    Remove logic to convert one timestamp to be the same as other

--- celery/utils/time.py
+++ celery/utils/time.py
@@ -221,8 +221,9 @@ def remaining(
     if str(
             start.tzinfo) == str(
             now.tzinfo) and now.utcoffset() != start.utcoffset():
-        # DST started/ended
-        start = start.replace(tzinfo=now.tzinfo)
+        if now.utcoffset() > start.utcoffset():
+            # DST started
+            start = start.replace(tzinfo=now.tzinfo)
     end_date = start + ends_in
     if relative:
         end_date = delta_resolution(end_date, ends_in).replace(microsecond=0)
