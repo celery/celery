0f5b003efb55ee964122e90561b323a92cb95452
Stevie <stevie.gayet@backmarket.com>
2023-01-20 16:27:33 +0100

    chore(tests): run tests on Python 3.11

--- setup.py
+++ setup.py
@@ -194,6 +194,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Operating System :: OS Independent"
