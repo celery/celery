370110c74b7505f528e902e81050c00afd3381f4
Dan Dougherty <dougherty@amyris.com>
2023-07-13 23:09:58 -0700

    ENH: Clean up test for preload options

---
+++ t/unit/app/test_preload_cli.py
@@ -0,0 +1,55 @@
+import sys
+from click.testing import CliRunner
+from celery.bin.celery import celery
+
+
+def test_preload_options(isolated_cli_runner: CliRunner):
+    # Verify commands like shell and purge can accept preload options.
+    # Projects like Pyramid-Celery's ini option should be valid preload
+    # options.
+
+    res_without_preload = isolated_cli_runner.invoke(
+        celery,
+        ["-A", "t.unit.bin.proj.app", "purge", "-f", "--ini", "some_ini.ini"],
+        catch_exceptions=True,
+    )
+
+    assert "No such option: --ini" in res_without_preload.stdout
+    assert res_without_preload.exit_code == 2
+
+    res_without_preload = isolated_cli_runner.invoke(
+        celery,
+        ["-A", "t.unit.bin.proj.app", "shell", "--ini", "some_ini.ini"],
+        catch_exceptions=True,
+    )
+
+    assert "No such option: --ini" in res_without_preload.stdout
+    assert res_without_preload.exit_code == 2
+
+    res_with_preload = isolated_cli_runner.invoke(
+        celery,
+        [
+            "-A",
+            "t.unit.bin.proj.pyramid_celery_app",
+            "purge",
+            "-f",
+            "--ini",
+            "some_ini.ini",
+        ],
+        catch_exceptions=True,
+    )
+
+    assert res_with_preload.exit_code == 0
+
+    res_with_preload = isolated_cli_runner.invoke(
+        celery,
+        [
+            "-A",
+            "t.unit.bin.proj.pyramid_celery_app",
+            "shell",
+            "--ini",
+            "some_ini.ini",
+        ],
+        catch_exceptions=True,
+    )
+    assert res_with_preload.exit_code == 0
