11c772b69280d29ccea68ecad4f8995fd124d0be
Jennifer Richards <jennifer@staff.ietf.org>
2023-09-01 13:38:29 -0300

    Isolate os.environ in test_cli_skip_checks

--- t/unit/bin/test_worker.py
+++ t/unit/bin/test_worker.py
@@ -1,4 +1,5 @@
 import os
+from unittest.mock import patch
 
 import pytest
 from click.testing import CliRunner
@@ -24,11 +25,11 @@ def test_cli(isolated_cli_runner: CliRunner):
 
 def test_cli_skip_checks(isolated_cli_runner: CliRunner):
     Logging._setup = True  # To avoid hitting the logging sanity checks
-    assert "CELERY_SKIP_CHECKS" not in os.environ, "CELERY_SKIP_CHECKS should not be set to start"
-    res = isolated_cli_runner.invoke(
-        celery,
-        ["-A", "t.unit.bin.proj.app", "--skip-checks", "worker", "--pool", "solo"],
-        catch_exceptions=False,
-    )
-    assert res.exit_code == 1, (res, res.stdout)
-    assert os.environ["CELERY_SKIP_CHECKS"] == "true", "should set CELERY_SKIP_CHECKS"
+    with patch.dict(os.environ, clear=True):
+        res = isolated_cli_runner.invoke(
+            celery,
+            ["-A", "t.unit.bin.proj.app", "--skip-checks", "worker", "--pool", "solo"],
+            catch_exceptions=False,
+        )
+        assert res.exit_code == 1, (res, res.stdout)
+        assert os.environ["CELERY_SKIP_CHECKS"] == "true", "should set CELERY_SKIP_CHECKS"
