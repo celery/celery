120b018bde83d42b09c9dba6badd3aa55f815f08
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2023-10-08 02:56:46 +0200

    Add tests

--- t/unit/tasks/test_tasks.py
+++ t/unit/tasks/test_tasks.py
@@ -1441,6 +1441,21 @@ class test_apply_task(TasksCase):
         with pytest.raises(KeyError):
             f.get()
 
+    def test_apply_eager_populates_request_task(self):
+        task_to_apply = self.task_check_request_context
+        with patch.object(
+            task_to_apply.request_stack, "push",
+            wraps=task_to_apply.request_stack.push,
+        ) as mock_push:
+            task_to_apply.apply()
+
+        mock_push.assert_called_once()
+
+        request = mock_push.call_args[0][0]
+
+        assert request.is_eager is True
+        assert request.task == 't.unit.tasks.test_tasks.task_check_request_context'
+
     def test_apply_simulates_delivery_info(self):
         task_to_apply = self.task_check_request_context
         with patch.object(
