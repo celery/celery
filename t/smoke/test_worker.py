c4a638f7c1b0333fb9754e06133634592febc33f
Tomer Nosrati <d6.tomer@gmail.com>
2023-10-15 22:36:13 +0300

    Fixed bug in test: test_reducing_prefetch_count()

--- t/smoke/test_worker.py
+++ t/smoke/test_worker.py
@@ -4,8 +4,9 @@ from pytest_celery import CeleryTestSetup
 from celery import Celery
 from t.smoke.tasks import long_running_task
 
-WORKER_PREFETCH_MULTIPLIER = 1
+WORKER_PREFETCH_MULTIPLIER = 2
 WORKER_CONCURRENCY = 3
+MAX_PREFETCH = WORKER_PREFETCH_MULTIPLIER * WORKER_CONCURRENCY
 
 
 @pytest.fixture
@@ -18,23 +19,24 @@ def default_worker_app(default_worker_app: Celery) -> Celery:
 
 class test_consumer:
     def test_reducing_prefetch_count(self, celery_setup: CeleryTestSetup):
-        worker_prefetch_multiplier = celery_setup.app.conf.worker_prefetch_multiplier
-        worker_concurrency = celery_setup.app.conf.worker_concurrency
-
-        assert worker_prefetch_multiplier == WORKER_PREFETCH_MULTIPLIER
-        assert worker_concurrency == WORKER_CONCURRENCY
+        assert celery_setup.app.conf.worker_prefetch_multiplier == WORKER_PREFETCH_MULTIPLIER
+        assert celery_setup.app.conf.worker_concurrency == WORKER_CONCURRENCY
 
         long_running_task.s().apply_async(queue=celery_setup.worker.worker_queue)
         celery_setup.broker.restart()
 
-        prefetch_reduce_message = (
-            f"Temporarily reducing the prefetch count to {worker_concurrency-worker_prefetch_multiplier} "
-            f"to avoid over-fetching since 1 tasks are currently being processed."
+        expected_running_tasks_count = 1
+        expected_reduced_prefetch = (WORKER_PREFETCH_MULTIPLIER * WORKER_CONCURRENCY) - (
+            WORKER_PREFETCH_MULTIPLIER * expected_running_tasks_count
         )
-        celery_setup.worker.wait_for_log(prefetch_reduce_message)
 
-        prefetch_restore_message = (
-            f"The prefetch count will be gradually restored to {worker_concurrency} "
-            f"as the tasks complete processing."
+        expected_prefetch_reduce_message = (
+            f"Temporarily reducing the prefetch count to {expected_reduced_prefetch} "
+            f"to avoid over-fetching since {expected_running_tasks_count} tasks are currently being processed."
         )
-        celery_setup.worker.wait_for_log(prefetch_restore_message)
+        celery_setup.worker.wait_for_log(expected_prefetch_reduce_message)
+
+        expected_prefetch_restore_message = (
+            f"The prefetch count will be gradually restored to {MAX_PREFETCH} " f"as the tasks complete processing."
+        )
+        celery_setup.worker.wait_for_log(expected_prefetch_restore_message)
