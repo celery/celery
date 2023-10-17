03e91b6cf89e69ab4e9521f429b87cc42a106a4b
Tomer Nosrati <tomer.nosrati@kcg.tech>
2023-10-17 22:56:58 +0300

    Pytest Celery Integration (#8241)
    * Added initial/sanity smoke tests
    
    * Allow using all integration tests tasks in the smoke tests environment, in addition to smoke tests specific tasks (to reuse existing tests tasks)
    
    * Added xdist support to smoke tests only
    
    * Added CI workflow for building the smoke tests dockerfiles
    
    * Added new tox env to clean resources & remains from the smoke tests: tox -e clean

---
+++ t/smoke/signals.py
@@ -0,0 +1,26 @@
+from celery.signals import worker_init, worker_process_init, worker_process_shutdown, worker_ready, worker_shutdown
+
+
+@worker_init.connect
+def worker_init_handler(sender, **kwargs):  # type: ignore
+    print("worker_init_handler")
+
+
+@worker_process_init.connect
+def worker_process_init_handler(sender, **kwargs):  # type: ignore
+    print("worker_process_init_handler")
+
+
+@worker_process_shutdown.connect
+def worker_process_shutdown_handler(sender, pid, exitcode, **kwargs):  # type: ignore
+    print("worker_process_shutdown_handler")
+
+
+@worker_ready.connect
+def worker_ready_handler(sender, **kwargs):  # type: ignore
+    print("worker_ready_handler")
+
+
+@worker_shutdown.connect
+def worker_shutdown_handler(sender, **kwargs):  # type: ignore
+    print("worker_shutdown_handler")
