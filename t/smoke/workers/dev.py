2926e131864417405c9729ed22fea84654f7b191
Tomer Nosrati <tomer.nosrati@kcg.tech>
2023-10-20 20:48:09 +0300

    Bugfix in test_prefetch_count_restored() and other enhancements  (#8580)
    * Fixed bug in test: test_prefetch_count_restored()
    
    * Changed all smoke tests workers log level from INFO to DEBUG
    
    * Changed usage of wait_for_log() -> assert_log_exists()

--- t/smoke/workers/dev.py
+++ t/smoke/workers/dev.py
@@ -19,7 +19,7 @@ class SmokeWorkerContainer(CeleryWorkerContainer):
 
     @classmethod
     def log_level(cls) -> str:
-        return "INFO"
+        return "DEBUG"
 
     @classmethod
     def worker_name(cls) -> str:
