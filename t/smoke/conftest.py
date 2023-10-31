5d06438f67cd5c6ee0dbb919f35fb467f5057285
Tomer Nosrati <tomer.nosrati@kcg.tech>
2023-11-01 01:10:47 +0200

    * Added t/smoke/test_tasks.py (#8599)
    * Added auto-session redis:latest container to smoke tests

--- t/smoke/conftest.py
+++ t/smoke/conftest.py
@@ -1,4 +1,8 @@
+import os
+
 import pytest
+from pytest_celery import REDIS_CONTAINER_TIMEOUT, REDIS_ENV, REDIS_IMAGE, REDIS_PORTS, RedisContainer
+from pytest_docker_tools import container, fetch, network
 
 from t.smoke.workers.dev import *  # noqa
 from t.smoke.workers.latest import *  # noqa
@@ -14,3 +18,21 @@ def default_worker_tasks() -> set:
         integration_tests_tasks,
         smoke_tests_tasks,
     }
+
+
+redis_image = fetch(repository=REDIS_IMAGE)
+redis_test_container_network = network(scope="session")
+redis_test_container: RedisContainer = container(
+    image="{redis_image.id}",
+    scope="session",
+    ports=REDIS_PORTS,
+    environment=REDIS_ENV,
+    network="{redis_test_container_network.name}",
+    wrapper_class=RedisContainer,
+    timeout=REDIS_CONTAINER_TIMEOUT,
+)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_redis_test_container(redis_test_container: RedisContainer):
+    os.environ["REDIS_PORT"] = str(redis_test_container.port)
--- t/smoke/tasks.py
+++ t/smoke/tasks.py
@@ -1,8 +1,10 @@
 from time import sleep
 
 import celery.utils
-from celery import shared_task
+from celery import Task, shared_task, signature
+from celery.canvas import Signature
 from t.integration.tasks import *  # noqa
+from t.integration.tasks import replaced_with_me
 
 
 @shared_task
@@ -11,5 +13,13 @@ def noop(*args, **kwargs) -> None:
 
 
 @shared_task
-def long_running_task(seconds: float = 1) -> None:
+def long_running_task(seconds: float = 1) -> bool:
     sleep(seconds)
+    return True
+
+
+@shared_task(bind=True)
+def replace_with_task(self: Task, replace_with: Signature = None):
+    if replace_with is None:
+        replace_with = replaced_with_me.s()
+    self.replace(signature(replace_with))
