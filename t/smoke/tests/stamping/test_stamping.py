73db1b1307f0c53a5e3bd7a6183ef8b710d81b8d
Tomer Nosrati <tomer.nosrati@kcg.tech>
2023-11-19 14:17:59 +0200

    Added test_stamping.py::test_multiple_stamps_multiple_workers()

--- t/smoke/tests/stamping/test_stamping.py
+++ t/smoke/tests/stamping/test_stamping.py
@@ -49,3 +49,39 @@ class test_stamping:
         worker: CeleryTestWorker
         for worker in celery_setup.worker_cluster:
             assert worker.logs().count(stamp)
+
+    def test_multiple_stamps_multiple_workers(self, celery_setup: CeleryTestSetup):
+        if len(celery_setup.worker_cluster) < 3:
+            pytest.skip("Not enough workers in cluster")
+
+        stamp = {"stamp": 420}
+        stamp1 = {**stamp, "stamp1": 4}
+        stamp2 = {**stamp, "stamp2": 2}
+        stamp3 = {**stamp, "stamp3": 42}
+
+        w1: CeleryTestWorker = celery_setup.worker_cluster[0]
+        w2: CeleryTestWorker = celery_setup.worker_cluster[1]
+        w3: CeleryTestWorker = celery_setup.worker_cluster[2]
+        stamped_task = chain(
+            identity.si(4).set(queue=w1.worker_queue).stamp(stamp1=stamp1["stamp1"]),
+            identity.si(2).set(queue=w2.worker_queue).stamp(stamp2=stamp2["stamp2"]),
+            identity.si(0).set(queue=w3.worker_queue).stamp(stamp3=stamp3["stamp3"]),
+        )
+        stamped_task.stamp(stamp=stamp["stamp"])
+        stamped_task.apply_async().get(timeout=RESULT_TIMEOUT)
+
+        stamp1 = json.dumps(stamp1, indent=4)
+        stamp2 = json.dumps(stamp2, indent=4)
+        stamp3 = json.dumps(stamp3, indent=4)
+
+        assert w1.logs().count(stamp1)
+        assert w1.logs().count(stamp2) == 0
+        assert w1.logs().count(stamp3) == 0
+
+        assert w2.logs().count(stamp1) == 0
+        assert w2.logs().count(stamp2)
+        assert w2.logs().count(stamp3) == 0
+
+        assert w3.logs().count(stamp1) == 0
+        assert w3.logs().count(stamp2) == 0
+        assert w3.logs().count(stamp3)
