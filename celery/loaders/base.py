347b310f930f9107b3c6fb3299325132be3dc2a7
John Whitman <jwhitman@galileo.io>
2023-11-20 20:12:38 -0500

    Defaults getattr to None.

--- celery/loaders/base.py
+++ celery/loaders/base.py
@@ -269,9 +269,9 @@ def find_related_module(package, related_name):
         # Try to find related_name under package.
         return importlib.import_module(module_name)
     except ModuleNotFoundError as e:
-        import_exc_name = getattr(e, 'name', module_name)
+        import_exc_name = getattr(e, 'name', None)
         # If candidate does not exist, then return None.
-        if module_name.startswith(import_exc_name):
+        if import_exc_name and module_name.startswith(import_exc_name):
             return
 
         # Otherwise, raise because error probably originated from a nested import.
