3436c3980f72824f7faae7c96a90540dd58fd1c9
Tomer Nosrati <tomer.nosrati@kcg.tech>
2023-11-22 17:17:47 +0200

    Added test_stamping.py::test_stamping_on_replace_with_legacy_worker_in_cluster()

--- t/smoke/tests/stamping/test_stamping.py
+++ t/smoke/tests/stamping/test_stamping.py
@@ -4,7 +4,9 @@ import pytest
 from pytest_celery import RESULT_TIMEOUT, CeleryTestSetup, CeleryTestWorker
 
 from celery.canvas import StampingVisitor, chain
-from t.integration.tasks import identity
+from t.integration.tasks import StampOnReplace, identity, replace_with_stamped_task
+from t.smoke.workers.dev import SmokeWorkerContainer
+from t.smoke.workers.legacy import CeleryLegacyWorkerContainer
 
 
 class test_stamping:
@@ -73,3 +75,55 @@ class test_stamping:
 
         assert w2.logs().count(stamp1) == 0
         assert w2.logs().count(stamp2)
+
+    def test_stamping_on_replace_with_legacy_worker_in_cluster(
+        self,
+        celery_setup: CeleryTestSetup,
+    ):
+        if len(celery_setup.worker_cluster) < 2:
+            pytest.skip("Not enough workers in cluster")
+
+        stamp = {"stamp": "Only for dev worker tasks"}
+        stamp1 = {**StampOnReplace.stamp, "stamp1": "1) Only for legacy worker tasks"}
+        stamp2 = {**StampOnReplace.stamp, "stamp2": "2) Only for legacy worker tasks"}
+
+        worker: CeleryTestWorker
+        for worker in celery_setup.worker_cluster:
+            if worker.version == SmokeWorkerContainer.version():
+                dev_worker = worker
+            elif worker.version == CeleryLegacyWorkerContainer.version():
+                legacy_worker = worker
+
+        replaced_sig1 = (
+            identity.si(4)
+            .set(queue=legacy_worker.worker_queue)
+            .stamp(stamp1=stamp1["stamp1"])
+        )
+        replaced_sig2 = (
+            identity.si(2)
+            .set(queue=legacy_worker.worker_queue)
+            .stamp(stamp2=stamp2["stamp2"])
+        )
+
+        stamped_task = chain(
+            replace_with_stamped_task.si(replace_with=replaced_sig1).set(
+                queue=dev_worker.worker_queue
+            ),
+            replace_with_stamped_task.si(replace_with=replaced_sig2).set(
+                queue=dev_worker.worker_queue
+            ),
+        )
+        stamped_task.stamp(stamp=stamp["stamp"])
+        stamped_task.apply_async().get(timeout=RESULT_TIMEOUT)
+
+        stamp = json.dumps(stamp, indent=4)
+        stamp1 = json.dumps(stamp1, indent=4)
+        stamp2 = json.dumps(stamp2, indent=4)
+
+        assert dev_worker.logs().count(stamp)
+        assert dev_worker.logs().count(stamp1) == 0
+        assert dev_worker.logs().count(stamp2) == 0
+
+        assert legacy_worker.logs().count(stamp) == 0
+        assert legacy_worker.logs().count(stamp1)
+        assert legacy_worker.logs().count(stamp2)
