87e1edc485fc89c1a1df9b36508880fa093b8bfa
Tomer Nosrati <tomer.nosrati@kcg.tech>
2023-11-23 12:58:33 +0200

    Hotfix + New smoke tests (#8664)
    * Changed smoke tests workers log level to INFO
    
    * Hotfix in t/smoke/tasks.py
    
    * Fixed missing teardown() call in latest & legacy workers in the smoke tests
    
    * Prefetch count smoke tests
    
    * Added t/smoke/test_control.py::test_shutdown_exit_with_zero()
    
    * Trigger CI tests on PR to smoke_tests branch.
    To be removed before merge to main!

--- t/smoke/test_consumer.py
+++ t/smoke/test_consumer.py
@@ -1,9 +1,9 @@
 import pytest
-from pytest_celery import CeleryTestSetup
+from pytest_celery import RESULT_TIMEOUT, CeleryTestSetup, RedisTestBroker
 
 from celery import Celery
 from celery.canvas import group
-from t.smoke.tasks import long_running_task
+from t.smoke.tasks import long_running_task, noop
 
 WORKER_PREFETCH_MULTIPLIER = 2
 WORKER_CONCURRENCY = 5
@@ -18,7 +18,13 @@ def default_worker_app(default_worker_app: Celery) -> Celery:
     yield app
 
 
-class test_consumer:
+class test_worker_enable_prefetch_count_reduction_true:
+    @pytest.fixture
+    def default_worker_app(self, default_worker_app: Celery) -> Celery:
+        app = default_worker_app
+        app.conf.worker_enable_prefetch_count_reduction = True
+        yield app
+
     @pytest.mark.parametrize("expected_running_tasks_count", range(1, WORKER_CONCURRENCY + 1))
     def test_reducing_prefetch_count(self, celery_setup: CeleryTestSetup, expected_running_tasks_count: int):
         sig = group(long_running_task.s(420) for _ in range(expected_running_tasks_count))
@@ -50,3 +56,45 @@ class test_consumer:
             f"Prefetch count has been restored to the maximum of {MAX_PREFETCH}"
         )
         celery_setup.worker.assert_log_exists(expected_prefetch_restore_message)
+
+    class test_cancel_tasks_on_connection_loss:
+        @pytest.fixture
+        def default_worker_app(self, default_worker_app: Celery) -> Celery:
+            app = default_worker_app
+            app.conf.worker_prefetch_multiplier = 2
+            app.conf.worker_cancel_long_running_tasks_on_connection_loss = True
+            app.conf.task_acks_late = True
+            yield app
+
+        def test_max_prefetch_passed_on_broker_restart(self, celery_setup: CeleryTestSetup):
+            if isinstance(celery_setup.broker, RedisTestBroker):
+                pytest.xfail("Real Bug: Broker does not fetch messages after restart")
+
+            sig = group(long_running_task.s(420) for _ in range(WORKER_CONCURRENCY))
+            sig.apply_async(queue=celery_setup.worker.worker_queue)
+            celery_setup.broker.restart()
+            noop.s().apply_async(queue=celery_setup.worker.worker_queue)
+            celery_setup.worker.assert_log_exists("Task t.smoke.tasks.noop")
+
+
+class test_worker_enable_prefetch_count_reduction_false:
+    @pytest.fixture
+    def default_worker_app(self, default_worker_app: Celery) -> Celery:
+        app = default_worker_app
+        app.conf.worker_prefetch_multiplier = 1
+        app.conf.worker_enable_prefetch_count_reduction = False
+        app.conf.worker_cancel_long_running_tasks_on_connection_loss = True
+        app.conf.task_acks_late = True
+        yield app
+
+    def test_max_prefetch_not_passed_on_broker_restart(self, celery_setup: CeleryTestSetup):
+        if isinstance(celery_setup.broker, RedisTestBroker):
+            pytest.xfail("Real Bug: Broker does not fetch messages after restart")
+
+        sig = group(long_running_task.s(10) for _ in range(WORKER_CONCURRENCY))
+        r = sig.apply_async(queue=celery_setup.worker.worker_queue)
+        celery_setup.broker.restart()
+        noop.s().apply_async(queue=celery_setup.worker.worker_queue)
+        assert "Task t.smoke.tasks.noop" not in celery_setup.worker.logs()
+        r.get(timeout=RESULT_TIMEOUT)
+        assert "Task t.smoke.tasks.noop" in celery_setup.worker.logs()
