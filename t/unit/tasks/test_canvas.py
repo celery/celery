33bb7a21a4ed1bd406a497f2411a4d7526b0d9a2
Wang Han <wanghan@airdoc.com>
2023-11-25 16:04:22 +0800

    add unit test

--- t/unit/tasks/test_canvas.py
+++ t/unit/tasks/test_canvas.py
@@ -591,6 +591,17 @@ class test_chain(CanvasCase):
         assert isinstance(c, _chain)
         assert len(c.tasks) == 1
 
+    def test_chain_of_chord_upgrade_on_chaining__protocol_4(self):
+        c = chain(
+            chain([self.add.s(i, i) for i in range(5)]),
+            group([self.add.s(i, i) for i in range(10, 15)], app=self.app),
+            chord([signature('header')], signature('body'), app=self.app),
+            group([self.add.s(i, i) for i in range(20, 25)], app=self.app)
+        )
+        assert isinstance(c, _chain)
+        assert len(c.tasks) == 6
+        assert isinstance(c.tasks[-1], chord)
+
     def test_apply_options(self):
 
         class static(Signature):
