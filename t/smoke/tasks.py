29b3557ea754150b7c27337f578c83e2174cb3c4
Tomer Nosrati <tomer.nosrati@kcg.tech>
2023-12-10 19:59:32 +0200

    Refactored worker smoke tests (#8708)

--- t/smoke/tasks.py
+++ t/smoke/tasks.py
@@ -20,6 +20,7 @@ def long_running_task(
     seconds: float = 1,
     verbose: bool = False,
     allocate: int | None = None,
+    exhaust_memory: bool = False,
 ) -> bool:
     from celery import current_task
     from celery.utils.log import get_task_logger
@@ -30,7 +31,12 @@ def long_running_task(
 
     if allocate:
         # Attempt to allocate megabytes in memory
-        _ = [0] * (allocate * 1024 * 1024 // getsizeof(int()))
+        _ = [0] * (allocate * 10**6 // getsizeof(int()))
+
+    if exhaust_memory:
+        mem = []
+        while True:
+            mem.append(' ' * 10**6)  # 1 MB of spaces
 
     for i in range(0, int(seconds)):
         sleep(1)
