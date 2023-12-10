97b7656348485f4e1f296419a8af562e736676bd
Tomer Nosrati <tomer.nosrati@kcg.tech>
2023-12-10 19:59:32 +0200

    Refactored worker smoke tests (#8708)

--- t/smoke/tests/test_worker.py
+++ t/smoke/tests/test_worker.py
@@ -4,17 +4,18 @@ from pytest_celery import RESULT_TIMEOUT, CeleryTestSetup
 from celery import Celery
 from celery.canvas import chain
 from t.smoke.tasks import long_running_task
+from t.smoke.tests.conftest import WorkerOperations
 
 
 @pytest.mark.parametrize(
     "restart_method",
     [
-        "pool_restart",
-        "docker_restart_gracefully",
-        "docker_restart_force",
+        WorkerOperations.RestartMethod.POOL_RESTART,
+        WorkerOperations.RestartMethod.DOCKER_RESTART_GRACEFULLY,
+        WorkerOperations.RestartMethod.DOCKER_RESTART_FORCE,
     ],
 )
-class test_worker_restart:
+class test_worker_restart(WorkerOperations):
     @pytest.fixture
     def default_worker_app(self, default_worker_app: Celery) -> Celery:
         app = default_worker_app
@@ -25,23 +26,18 @@ class test_worker_restart:
     def test_restart_during_task_execution(
         self,
         celery_setup: CeleryTestSetup,
-        restart_method: str,
+        restart_method: WorkerOperations.RestartMethod,
     ):
         queue = celery_setup.worker.worker_queue
         sig = long_running_task.si(5, verbose=True).set(queue=queue)
         res = sig.delay()
-        if restart_method == "pool_restart":
-            celery_setup.app.control.pool_restart()
-        elif restart_method == "docker_restart_gracefully":
-            celery_setup.worker.restart()
-        elif restart_method == "docker_restart_force":
-            celery_setup.worker.restart(force=True)
+        self.restart(celery_setup, restart_method)
         assert res.get(RESULT_TIMEOUT) is True
 
     def test_restart_between_task_execution(
         self,
         celery_setup: CeleryTestSetup,
-        restart_method: str,
+        restart_method: WorkerOperations.RestartMethod,
     ):
         queue = celery_setup.worker.worker_queue
         first = long_running_task.si(5, verbose=True).set(queue=queue)
@@ -51,10 +47,5 @@ class test_worker_restart:
         sig = chain(first, second)
         sig.delay()
         assert first_res.get(RESULT_TIMEOUT) is True
-        if restart_method == "pool_restart":
-            celery_setup.app.control.pool_restart()
-        elif restart_method == "docker_restart_gracefully":
-            celery_setup.worker.restart()
-        elif restart_method == "docker_restart_force":
-            celery_setup.worker.restart(force=True)
+        self.restart(celery_setup, restart_method)
         assert second_res.get(RESULT_TIMEOUT) is True
