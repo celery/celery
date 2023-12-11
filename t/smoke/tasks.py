9a6501d806e5ca8bb53d8cd342298ff2bec21da5
Tomer Nosrati <tomer.nosrati@kcg.tech>
2023-12-11 22:15:38 +0200

    Refactored worker smoke tests utilities (#8712)
    * Fixed imports in smoke tests
    
    * Refactored WorkerOperations in smoke tests
    
    * Use dataclass for worker termination operation options instead of plain dict
    
    * Using get(timeout=RESULT_TIMEOUT)
    
    * Reload worker container obj after termination/restart
    
    * Added cleanup to suicide_exhaust_hdd()
    
    * Reverted "Run all tests in CI together (to be reverted)"
    
    * Run smoke tests CI only after integration tests (finally)
    
    * --reruns-delay 10 -> 60 for smoke tests
    
    * BaseException -> Exception
    
    * Disabled Redis Broker in Smoke Tests - Redis Broker feature is too unstable
    
    * Improved stability of smoke tests
    
    * Configure back Redis Broker for smoke tests
    
    * Cleanup and renaming
    
    * Added TODO
    
    * t/smoke --reruns 10 --reruns-delay 60 --rerun-except AssertionError
    
    * Renamed WorkerOperations -> SuiteOperations
    
    * Refactored SuiteOperations code into separated modules

--- t/smoke/tasks.py
+++ t/smoke/tasks.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 
+import math
+import os
+import sys
+from signal import SIGKILL
 from sys import getsizeof
 from time import sleep
 
 import celery.utils
 from celery import Task, shared_task, signature
+from celery.app.control import Control
 from celery.canvas import Signature
 from t.integration.tasks import *  # noqa
 from t.integration.tasks import replaced_with_me
@@ -16,27 +21,13 @@ def noop(*args, **kwargs) -> None:
 
 
 @shared_task
-def long_running_task(
-    seconds: float = 1,
-    verbose: bool = False,
-    allocate: int | None = None,
-    exhaust_memory: bool = False,
-) -> bool:
+def long_running_task(seconds: float = 1, verbose: bool = False) -> bool:
     from celery import current_task
     from celery.utils.log import get_task_logger
 
     logger = get_task_logger(current_task.name)
 
     logger.info("Starting long running task")
-
-    if allocate:
-        # Attempt to allocate megabytes in memory
-        _ = [0] * (allocate * 10**6 // getsizeof(int()))
-
-    if exhaust_memory:
-        mem = []
-        while True:
-            mem.append(' ' * 10**6)  # 1 MB of spaces
 
     for i in range(0, int(seconds)):
         sleep(1)
@@ -53,3 +44,118 @@ def replace_with_task(self: Task, replace_with: Signature = None):
     if replace_with is None:
         replace_with = replaced_with_me.s()
     return self.replace(signature(replace_with))
+
+
+@shared_task
+def suicide(method: str, try_eager: bool = True, **options: dict):
+    termination_method = {
+        "DELAY_TIMEOUT": suicide_delay_timeout.si(
+            time_limit=options["time_limit"],
+        ),
+        "CPU_OVERLOAD": suicide_cpu_overload.si(
+            cpu_load_factor=options["cpu_load_factor"]
+        ),
+        "EXCEPTION": suicide_exception.si(),
+        "SYSTEM_EXIT": suicide_system_exit.si(),
+        "ALLOCATE_MAX_MEMORY": suicide_allocate_max_memory.si(
+            allocate=options["allocate"]
+        ),
+        "EXHAUST_MEMORY": suicide_exhaust_memory.si(),
+        "EXHAUST_HDD": suicide_exhaust_hdd.si(
+            large_file_name=options["large_file_name"]
+        ),
+        "CONTROL_SHUTDOWN": suicide_control_shutdown.si(
+            hostname=options["hostname"],
+        ),
+        "SIGKILL": suicide_sigkill.si(),
+    }
+
+    sig = termination_method.get(method)
+    if sig:
+        if try_eager and method in {
+            "CONTROL_SHUTDOWN",
+        }:
+            return sig.apply().get()
+
+        worker = options["worker"]
+        return sig.apply_async(queue=worker.worker_queue).get()
+    else:
+        raise ValueError(f"Unsupported termination method: {method}")
+
+
+@shared_task(time_limit=2)
+def suicide_delay_timeout(time_limit: int = 4):
+    """Delays the execution to simulate a task timeout."""
+    sleep(time_limit)
+
+
+@shared_task
+def suicide_cpu_overload(cpu_load_factor: int = 420):
+    """Performs CPU-intensive operations to simulate a CPU overload."""
+
+    def cpu_intensive_calculation(n):
+        return cpu_intensive_calculation(math.sin(n))
+
+    cpu_intensive_calculation(cpu_load_factor)
+
+
+@shared_task
+def suicide_exception():
+    """Raises an exception to simulate an unexpected error during task execution."""
+    raise Exception("Simulated task failure due to an exception.")
+
+
+@shared_task
+def suicide_system_exit():
+    """Triggers a system exit to simulate a critical stop of the Celery worker."""
+    sys.exit("Simulated Celery worker stop via system exit.")
+
+
+@shared_task
+def suicide_allocate_max_memory(allocate: int):
+    """Allocates the maximum amount of memory permitted, potentially leading to memory errors."""
+    _ = [0] * (allocate // getsizeof(int()))
+
+
+@shared_task
+def suicide_exhaust_memory():
+    """Continuously allocates memory to simulate memory exhaustion."""
+    mem = []
+    while True:
+        mem.append(" " * 10**6)
+
+
+@shared_task
+def suicide_exhaust_hdd(large_file_name: str = "large_file"):
+    """Consumes disk space in /tmp to simulate a scenario where the disk is getting full."""
+    # file_path = f"/tmp/{large_file_name}.tmp"
+    # try:
+    #     with open(file_path, "wb") as f:
+    #         chunk = b"\0" * 42 * 1024**2  # 42 MB
+    #         while True:
+    #             f.write(chunk)
+    # finally:
+    #     if os.path.exists(file_path):
+    #         os.remove(file_path)
+
+    # This code breaks GitHub CI so we simulate the same error as best effort
+    #########################################################################
+    # [error]Failed to create step summary using 'GITHUB_STEP_SUMMARY': No space left on device
+    # [error]No space left on device
+    raise OSError("No space left on device")
+
+
+@shared_task
+def suicide_control_shutdown(hostname: str):
+    """Initiates a controlled shutdown via the Control API."""
+    from celery.app.base import get_current_app
+
+    app = get_current_app()
+    control: Control = app.control
+    control.shutdown(destination=[hostname])
+
+
+@shared_task
+def suicide_sigkill():
+    """Forceful termination."""
+    os.kill(os.getpid(), SIGKILL)
--- t/smoke/tests/test_worker.py
+++ t/smoke/tests/test_worker.py
@@ -3,19 +3,12 @@ from pytest_celery import RESULT_TIMEOUT, CeleryTestSetup
 
 from celery import Celery
 from celery.canvas import chain
+from t.smoke.conftest import SuiteOperations, WorkerRestart
 from t.smoke.tasks import long_running_task
-from t.smoke.tests.conftest import WorkerOperations
 
 
-@pytest.mark.parametrize(
-    "restart_method",
-    [
-        WorkerOperations.RestartMethod.POOL_RESTART,
-        WorkerOperations.RestartMethod.DOCKER_RESTART_GRACEFULLY,
-        WorkerOperations.RestartMethod.DOCKER_RESTART_FORCE,
-    ],
-)
-class test_worker_restart(WorkerOperations):
+@pytest.mark.parametrize("method", list(WorkerRestart.Method))
+class test_worker_restart(SuiteOperations):
     @pytest.fixture
     def default_worker_app(self, default_worker_app: Celery) -> Celery:
         app = default_worker_app
@@ -26,18 +19,18 @@ class test_worker_restart(WorkerOperations):
     def test_restart_during_task_execution(
         self,
         celery_setup: CeleryTestSetup,
-        restart_method: WorkerOperations.RestartMethod,
+        method: WorkerRestart,
     ):
         queue = celery_setup.worker.worker_queue
         sig = long_running_task.si(5, verbose=True).set(queue=queue)
         res = sig.delay()
-        self.restart(celery_setup, restart_method)
+        self.restart_worker(celery_setup.worker, method)
         assert res.get(RESULT_TIMEOUT) is True
 
     def test_restart_between_task_execution(
         self,
         celery_setup: CeleryTestSetup,
-        restart_method: WorkerOperations.RestartMethod,
+        method: WorkerRestart,
     ):
         queue = celery_setup.worker.worker_queue
         first = long_running_task.si(5, verbose=True).set(queue=queue)
@@ -47,5 +40,5 @@ class test_worker_restart(WorkerOperations):
         sig = chain(first, second)
         sig.delay()
         assert first_res.get(RESULT_TIMEOUT) is True
-        self.restart(celery_setup, restart_method)
+        self.restart_worker(celery_setup.worker, method)
         assert second_res.get(RESULT_TIMEOUT) is True
