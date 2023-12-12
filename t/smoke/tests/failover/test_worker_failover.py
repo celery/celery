0c955df972bde9e1f05733445da74ec7450bc9a9
Tomer Nosrati <tomer.nosrati@kcg.tech>
2023-12-12 13:56:26 +0200

    Removed useless test: test_task_retry_on_worker_crash()

--- t/smoke/tests/failover/test_worker_failover.py
+++ t/smoke/tests/failover/test_worker_failover.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import pytest
-from pytest_celery import RESULT_TIMEOUT, CeleryTestSetup, CeleryTestWorker, CeleryWorkerCluster, RedisTestBroker
+from pytest_celery import RESULT_TIMEOUT, CeleryTestSetup, CeleryTestWorker, CeleryWorkerCluster
 
 from celery import Celery
 from t.smoke.conftest import SuiteOperations, WorkerKill
@@ -58,28 +58,10 @@ class test_worker_failover(SuiteOperations):
         queue = celery_setup.worker.worker_queue
         sig = long_running_task.si(1).set(queue=queue)
         res = sig.delay()
-        assert res.get(timeout=10) is True
+        assert res.get(timeout=RESULT_TIMEOUT) is True
         for worker in celery_setup.worker_cluster:
             self.kill_worker(worker, method)
         celery_setup.worker.restart()
         sig = long_running_task.si(1).set(queue=queue)
         res = sig.delay()
-        assert res.get(timeout=10) is True
-
-    def test_task_retry_on_worker_crash(
-        self,
-        celery_setup: CeleryTestSetup,
-        method: WorkerKill,
-    ):
-        assert len(celery_setup.worker_cluster) > 1
-
-        if isinstance(celery_setup.broker, RedisTestBroker):
-            pytest.xfail("Potential Bug: works with RabbitMQ, but not Redis")
-
-        sleep_time = 4
-        queue = celery_setup.worker.worker_queue
-        sig = long_running_task.si(sleep_time, verbose=True).set(queue=queue)
-        res = sig.apply_async(retry=True, retry_policy={"max_retries": 1})
-        celery_setup.worker.wait_for_log("Sleeping: 2")  # Let task run
-        self.kill_worker(celery_setup.worker, method)
-        assert res.get(timeout=10) is True
+        assert res.get(timeout=RESULT_TIMEOUT) is True
