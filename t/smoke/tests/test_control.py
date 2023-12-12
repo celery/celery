15bf48d1ba8770269e26e9f31f004d0a072d2668
Tomer Nosrati <tomer.nosrati@kcg.tech>
2023-12-12 18:29:09 +0200

    Hotfix (#8717)
    * Removed useless test: test_task_retry_on_worker_crash()
    
    * Completed TODO in test_shutdown_exit_with_zero()
    
    * Increased worker memory for test_worker_failover from 10MB to 100MB
    
    * Updated pytest-xdist to v3.5+

--- t/smoke/tests/test_control.py
+++ t/smoke/tests/test_control.py
@@ -12,8 +12,7 @@ class test_control:
         )
 
     def test_shutdown_exit_with_zero(self, celery_setup: CeleryTestSetup):
-        # TODO: celery_setup.app.control.shutdown(destination=[celery_setup.worker.hostname()])
-        celery_setup.app.control.shutdown()
+        celery_setup.app.control.shutdown(destination=[celery_setup.worker.hostname()])
         while celery_setup.worker.container.status != "exited":
             celery_setup.worker.container.reload()
         assert celery_setup.worker.container.attrs["State"]["ExitCode"] == 0
