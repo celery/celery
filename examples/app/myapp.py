328d09180aa91993ce7903a295b67dfcd5385a11
Omer Katz <omer.katz@kcg.tech>
2024-04-10 19:25:42 +0300

    myapp changes for debugging purposes

--- examples/app/myapp.py
+++ examples/app/myapp.py
@@ -3,8 +3,36 @@ from celery.canvas import chain, group
 
 app = Celery(
     "myapp",
-    broker="redis://localhost/1",
+    broker="amqp://localhost:5673",
 )
+
+app.conf.update(
+        accept_content=["json"],
+        task_serializer="json",
+        result_serializer="json",
+        event_serializer="json",
+        # broker_url=get_broker_url(ssl=False),
+        broker_use_ssl=False,
+        broker_transport_options={"confirm_publish": True},
+        broker_login_method="PLAIN",
+        # result_backend="dbcache+bas://dlj2backendsvc-1.4",
+        worker_prefetch_multiplier=1,
+        worker_concurrency=16,
+        task_acks_late=False,
+        task_reject_on_worker_lost=True,
+        task_result_expires=60 * 60 * 24,  # 24 hours
+        task_publish_retry_policy={
+            # "errback": kombu_error_handler,
+            "max_retries": 20,
+            "interval_start": 1,
+            "interval_step": 2,
+            "interval_max": 30,
+            # "retry_errors": (MessageNacked,),
+        },
+        task_default_delivery_mode=1,
+        worker_enable_remote_control=False,
+        worker_cancel_long_running_tasks_on_connection_loss=True,
+    )
 
 
 @app.task
