cec53036e0bcea4b6d3e6e747121cae8539fe3c4
Asif Saif Uddin <auvipy@gmail.com>
2024-05-26 12:56:50 +0600

    Update celery/schedules.py
    Co-authored-by: Johannes Faigle <jo-f@gmx.at>

--- celery/schedules.py
+++ celery/schedules.py
@@ -413,7 +413,7 @@ class crontab(BaseSchedule):
         """
         Create a Crontab from a string. For example ``crontab='* * * * *'``.
         """
-        minute, hour, day_of_week, day_of_month, month_of_year = crontab.split(" ")
+        minute, hour, day_of_month, month_of_year, day_of_week = crontab.split(" ")
         return cls(minute, hour, day_of_week, day_of_month, month_of_year)
 
     @staticmethod
