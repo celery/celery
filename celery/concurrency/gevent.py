b8da159c72592f7faa8c20680f9f9aec70d8f068
ZHOU Cheng <c.zhou@live.com>
2024-06-19 18:40:16 +0800

    apply target

--- celery/concurrency/gevent.py
+++ celery/concurrency/gevent.py
@@ -16,6 +16,13 @@ __all__ = ('TaskPool',)
 # We cache globals and attribute lookups, so disable this warning.
 
 
+def apply_target(target, args=(), kwargs=None, callback=None,
+                 accept_callback=None, getpid=None):
+    kwargs = {} if not kwargs else kwargs
+    return base.apply_target(target, args, kwargs, callback, accept_callback,
+                             pid=getpid())
+
+
 def apply_timeout(target, args=(), kwargs=None, callback=None,
                   accept_callback=None, pid=None, timeout=None,
                   timeout_callback=None, Timeout=Timeout,
