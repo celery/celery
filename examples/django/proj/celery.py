60b2bc8d87089a6495d30f4611710eb2a691fcad
Giovanni <63993401+giovanni1106@users.noreply.github.com>
2024-06-26 00:07:09 -0300

    docs: add missing import

--- examples/django/proj/celery.py
+++ examples/django/proj/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from django.conf import settings
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
