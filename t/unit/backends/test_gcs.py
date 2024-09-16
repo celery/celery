e3f74b9ab027516a634efa0988ee4dee6343d1af
Tomer Nosrati <tomer.nosrati@gmail.com>
2024-09-16 12:09:49 +0300

    Fixed pypy unit tests random failures in the CI

--- t/unit/backends/test_gcs.py
+++ t/unit/backends/test_gcs.py
@@ -145,7 +145,7 @@ class test_GCSBackend:
         backend = GCSBackend(app=self.app)
         mock_get.side_effect = ['value1', 'value2']
         result = backend.mget([b'key1', b'key2'])
-        mock_get.assert_has_calls([call(b'key1'), call(b'key2')])
+        mock_get.assert_has_calls([call(b'key1'), call(b'key2')], any_order=True)
         assert result == ['value1', 'value2']
 
     @patch('celery.backends.gcs.Client')
