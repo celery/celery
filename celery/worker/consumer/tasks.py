e1a18ee2da694633f61314a75fd7704a1534abbc
Omer Katz <omer.katz@kcg.tech>
2024-10-13 19:24:59 +0300

    Delayed native delivery queues can now be non-quorum queues.

--- celery/worker/consumer/tasks.py
+++ celery/worker/consumer/tasks.py
@@ -93,9 +93,6 @@ class Tasks(bootsteps.StartStopStep):
         if c.app.conf.worker_detect_quorum_queues:
             using_quorum_queues, qname = self.detect_quorum_queues(c)
 
-            if c.app.conf.broker_native_delayed_delivery is True:
-                using_quorum_queues = True
-
             if using_quorum_queues:
                 qos_global = False
                 # The ETA tasks mechanism requires additional work for Celery to fully support
