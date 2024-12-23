71cff091ec3829b958ad95327fbf1979a171f70b
Ryusuke Nagai <rnagai@lycorp.co.jp>
2024-12-23 18:19:08 +0900

    test: add assertions for CassandraBackend port configuration

--- t/unit/backends/test_cassandra.py
+++ t/unit/backends/test_cassandra.py
@@ -267,4 +267,11 @@ class test_CassandraBackend:
             'cql_version': '3.2.1',
             'protocol_version': 3
         }
-        mod.CassandraBackend(app=self.app)
+        x = mod.CassandraBackend(app=self.app)
+        # Default port is 9042
+        assert x.port == 9042
+
+        # Valid options with port specified
+        self.app.conf.cassandra_port = 1234
+        x = mod.CassandraBackend(app=self.app)
+        assert x.port == 1234
