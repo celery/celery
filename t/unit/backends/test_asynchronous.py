b1b32c1e288c30a4a34d81281d60d6817a235644
Linus Phan <13613724+linusphan@users.noreply.github.com>
2025-07-10 17:30:32 -0700

    Fix bare except clause in test teardown_thread method

--- t/unit/backends/test_asynchronous.py
+++ t/unit/backends/test_asynchronous.py
@@ -212,12 +212,7 @@ class test_EventletDrainer(GreenletDrainerTests):
         return g
 
     def teardown_thread(self, thread):
-        try:
-            # eventlet's API acts like a join() rather
-            # than wait, and throws if the greenlet threw
-            thread.wait()
-        except Exception:
-            pass
+        thread.kill()
 
 
 class test_Drainer(DrainerTests):
