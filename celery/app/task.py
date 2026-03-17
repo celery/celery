758d17f2ea6c2d2ed22b15f768bbe8288070d3bf
bennyhsiao <bennyhsiao@synology.com>
2026-03-17 03:14:52 +0000

    fix: avoid timelimt is missing

--- celery/app/task.py
+++ celery/app/task.py
@@ -890,8 +890,10 @@ class Task:
             'callbacks': maybe_list(link),
             'errbacks': maybe_list(link_error),
             'headers': headers,
-            'timelimit': None if self.time_limit is None and self.soft_time_limit is None
-                         else [self.time_limit, self.soft_time_limit],
+            'timelimit': (
+                None if self.time_limit is None and self.soft_time_limit is None
+                else [self.time_limit, self.soft_time_limit]
+            ),
             'ignore_result': options.get('ignore_result', False),
             'delivery_info': {
                 'is_eager': True,
