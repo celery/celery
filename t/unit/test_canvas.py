48a55f56b3839f6d72c065477e24915a3a1e93eb
Noa Levi <275430404+lphuc2250gma@users.noreply.github.com>
2026-06-08 16:11:22 +0000

    chore: improve celery maintenance path

--- t/unit/test_canvas.py
+++ t/unit/test_canvas.py
@@ -31,3 +31,16 @@ class test_Canvas:
         assert t_reply_to_app == t_reply_to_opt
         # reply_to must be thread-relative.
         assert t_reply_to_opt != s.options['reply_to']
+
+    def test_freeze_preserves_existing_reply_to(self):
+        # freeze() must not overwrite reply_to if already present in options
+
+        @self.app.task
+        def test_task(a, b):
+            return
+
+        s = test_task.s(2, 2)
+        s.options['reply_to'] = 'custom-reply-to'
+        s.freeze()
+
+        assert s.options['reply_to'] == 'custom-reply-to'
