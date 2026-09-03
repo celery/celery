68df7d1df6290c5f2f8dcfcd840f35af6d0dac90
Asif Saif Uddin <auvipy@gmail.com>
2026-09-03 15:04:28 +0600

    update permission of home directory

--- docker/scripts/create-linux-user.sh
+++ docker/scripts/create-linux-user.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 set -eu
 
 addgroup --gid 1000 "$CELERY_USER"
@@ -9,4 +8,7 @@ adduser \
     --disabled-password \
     --uid 1000 \
     --gid 1000 \
+    --home "/home/$CELERY_USER" \
     "$CELERY_USER"
+
+chown -R 1000:1000 "/home/$CELERY_USER"
