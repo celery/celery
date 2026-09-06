6e58e3febcaa396f35f8d6f00ebbff81ad821d18
phanky1 <phanky.vn@proton.me>
2026-09-06 17:13:41 +0700

    Return Option objects consistently from find()

--- celery/app/defaults.py
+++ celery/app/defaults.py
@@ -403,8 +403,9 @@ def flatten(d, root='', keyfilter=_flatten_keys):
                 yield from keyfilter(ns, key, opt)
 
 
+_OPTIONS = dict(flatten(NAMESPACES))
 DEFAULTS = {
-    key: opt.default for key, opt in flatten(NAMESPACES)
+    key: opt.default for key, opt in _OPTIONS.items()
 }
 __compat = list(flatten(NAMESPACES, keyfilter=_to_compat))
 _OLD_DEFAULTS = {old_key: opt.default for old_key, _, opt in __compat}
@@ -447,4 +448,4 @@ def find(name, namespace='celery'):
                 except KeyError:
                     pass
     # - See if name is a qualname last.
-    return searchresult(None, name.lower(), DEFAULTS[name.lower()])
+    return searchresult(None, name.lower(), _OPTIONS[name.lower()])
